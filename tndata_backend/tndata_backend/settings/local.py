0d49996203f8d866e351a71ecbefb0c021c6f4b5
Brad Montgomery <brad@bradmontgomery.net>
2014-11-12 17:30:13 -0600

    updated settings

--- tndata_backend/tndata_backend/settings/local.py
+++ tndata_backend/tndata_backend/settings/local.py
@@ -4,15 +4,6 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
