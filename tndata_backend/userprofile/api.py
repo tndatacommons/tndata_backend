8833bcc75eb22b20803123f042505ec90261fa75
Brad Montgomery <brad@bradmontgomery.net>
2014-11-30 17:31:36 -0600

    permissions for user/userprofile api endpoints

--- tndata_backend/userprofile/api.py
+++ tndata_backend/userprofile/api.py
@@ -1,27 +1,46 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers, viewsets
+from rest_framework import permissions, serializers, viewsets
 
 from . import models
 
 
-# Serializers define API representation
+class IsSelf(permissions.BasePermission):
+    """This permission checks for a User/UserProfile's owner."""
+
+    def has_object_permission(self, request, view, obj):
+        try:
+            return obj.user == request.user
+        except AttributeError:
+            return obj == request.user
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'is_staff')
+        fields = ('id', 'username', 'email', 'is_staff')
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsSelf]
+
+    def get_queryset(self):
+        return self.queryset.filter(id=self.request.user.id)
 
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.UserProfile
-        fields = ('user', 'birthdate', 'race', 'gender', 'marital_status', )
+        fields = (
+            'id', 'user', 'birthdate', 'race', 'gender', 'marital_status',
+        )
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated, IsSelf]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
