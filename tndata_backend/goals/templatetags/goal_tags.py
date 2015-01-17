f31736b11ddd0afa128483d322bdb09d9880e7c3
Brad Montgomery <brad@bradmontgomery.net>
2015-01-16 18:21:36 -0600

    build an object_controls template tag for links to edit/delete objects

---
+++ tndata_backend/goals/templatetags/goal_tags.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("goals/_object_controls.html")
+def object_controls(obj, label=None):
+    """Renders a dropdown button with `Update` and `Delete` Links for the given
+    object. Include an optional label for multiple controls on different types
+    of objects. If label is omitted, the object's class name is used.
+
+    NOTE: objects must implement `get_update_url` and `get_delete_url`.
+
+    """
+    if label is None:
+        label = obj.__class__.__name__.lower()
+    return {'label': label, 'object': obj}
