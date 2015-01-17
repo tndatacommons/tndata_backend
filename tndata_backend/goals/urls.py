ccf743761b69e648c7f68ecd793a6591e7dcd053
Brad Montgomery <brad@bradmontgomery.net>
2015-01-17 16:01:48 -0600

    added CRUD views for Interest objects; displaying existing Categories on the edit/update form.

--- tndata_backend/goals/urls.py
+++ tndata_backend/goals/urls.py
@@ -8,11 +8,38 @@ urlpatterns = patterns('',
         views.CategoryList.as_view(),
         name='index'
     ),
+
     url(
         r'new/category/$',
         views.CategoryCreateView.as_view(),
         name='category-create'
     ),
+    url(
+        r'new/interest/$',
+        views.InterestCreateView.as_view(),
+        name='interest-create'
+    ),
+
+    # TODO: Crud for Actions
+
+    # Crud for Interests
+    url(
+        r'interest/(?P<name_slug>.+)/update/$',
+        views.InterestUpdateView.as_view(),
+        name='interest-update'
+    ),
+    url(
+        r'interest/(?P<name_slug>.+)/delete/$',
+        views.InterestDeleteView.as_view(),
+        name='interest-delete'
+    ),
+    url(
+        r'interest/(?P<name_slug>.+)/$',
+        views.InterestDetailView.as_view(),
+        name='interest-detail'
+    ),
+
+    # crud for Category
     url(
         r'(?P<name_slug>.+)/update/$',
         views.CategoryUpdateView.as_view(),
--- tndata_backend/goals/views.py
+++ tndata_backend/goals/views.py
@@ -2,7 +2,7 @@ from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
-from . models import Category
+from . models import Category, Interest
 
 
 class CategoryList(ListView):
@@ -21,6 +21,11 @@ class CategoryCreateView(CreateView):
     model = Category
     fields = ['order', 'name', 'description']
 
+    def get_context_data(self, **kwargs):
+        context = super(CategoryCreateView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class CategoryUpdateView(UpdateView):
     model = Category
@@ -28,9 +33,55 @@ class CategoryUpdateView(UpdateView):
     slug_url_kwarg = "name_slug"
     fields = ['order', 'name', 'description']
 
+    def get_context_data(self, **kwargs):
+        context = super(CategoryUpdateView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class CategoryDeleteView(DeleteView):
     model = Category
+    slug_field = "name_slug"
+    slug_url_kwarg = "name_slug"
+    success_url = reverse_lazy('goals:index')
+
+
+class InterestDetailView(DetailView):
+    queryset = Interest.objects.all()
+    slug_field = "name_slug"
+    slug_url_kwarg = "name_slug"
+
+
+class InterestCreateView(CreateView):
+    model = Interest
+    fields = [
+        'order', 'name', 'description', 'categories',
+        'max_neef_tags', 'sdt_major'
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super(InterestCreateView, self).get_context_data(**kwargs)
+        context['interests'] = Interest.objects.all()
+        return context
+
+
+class InterestUpdateView(UpdateView):
+    model = Interest
+    slug_field = "name_slug"
+    slug_url_kwarg = "name_slug"
+    fields = [
+        'order', 'name', 'description', 'categories',
+        'max_neef_tags', 'sdt_major'
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super(InterestUpdateView, self).get_context_data(**kwargs)
+        context['interests'] = Interest.objects.all()
+        return context
+
+
+class InterestDeleteView(DeleteView):
+    model = Interest
     slug_field = "name_slug"
     slug_url_kwarg = "name_slug"
     success_url = reverse_lazy('goals:index')
