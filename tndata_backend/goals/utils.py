f6e7f8336c65f651c5d5297b0db6ddddde3f28cc
Brad Montgomery <brad@bradmontgomery.net>
2015-01-20 23:15:45 -0600

    added a function to read from an uploaded CSV file

--- tndata_backend/goals/utils.py
+++ tndata_backend/goals/utils.py
@@ -1,12 +1,37 @@
+import csv
 from django.db.models import Max
+from io import TextIOWrapper
 
 
 def get_max_order(model):
     """Given an instance of a Model class, query the largest existing value
-    for the `order` field and add one. This is useful for pre-populated Category,
-    Interest, and Action fields.
+    for the `order` field and add one. This is useful for pre-populated
+    Category, Interest, and Action fields.
 
     """
     result = model.objects.aggregate(Max('order'))
     current_num = result['order__max'] or 0
     return current_num + 1
+
+
+def read_csv(uploaded_file, encoding='utf-8', errors='ignore'):
+    """This is a generator that takes an uploaded file (such as an instance of
+    InMemoryUploadedFile.file), converts it to a string (instead of bytes)
+    representation, then parses it as a CSV.
+
+    InMemoryUploadedFileSee: http://stackoverflow.com/a/16243182/182778
+
+    Returns a list of lists containing strings.
+
+    **NOTE**: This makes a big assumption about utf-8 encodings, and the errors
+    param means we potentially lose data!
+
+    """
+    file = TextIOWrapper(
+        uploaded_file.file,
+        encoding=encoding,
+        newline='',
+        errors=errors
+    )
+    for row in csv.reader(file):
+        yield row
