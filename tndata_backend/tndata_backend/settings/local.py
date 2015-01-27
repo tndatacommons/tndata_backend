57cf46e8f3e24c1507a58b655644759b1636a98d
Brad Montgomery <brad@bradmontgomery.net>
2015-01-27 16:21:58 -0600

    removing local db settings and simplifying media url

--- tndata_backend/tndata_backend/settings/local.py
+++ tndata_backend/tndata_backend/settings/local.py
@@ -4,19 +4,6 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-# NOTE: config for local (osx) Postgresql.app (not vagrant)
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'tndata',
-        #'USER': 'brad',
-        #'PASSWORD': '',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
-    #}
-#}
-
-
 INSTALLED_APPS = INSTALLED_APPS + (
     'debug_toolbar',
 )
@@ -24,7 +11,7 @@ INSTALLED_APPS = INSTALLED_APPS + (
 # Media Uploads for Development
 # NOTE. during dev, cd into uploads and run `serve 8888`
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-MEDIA_URL = "http://localhost:8888/"
+MEDIA_URL = "/media/"
 
 # django-core-headers
 # https://github.com/ottoyiu/django-cors-headers/
