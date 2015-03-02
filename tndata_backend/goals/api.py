70665f280d818915bbffcacf6d5a6913ec03d756
Brad Montgomery <brad@bradmontgomery.net>
2015-03-02 10:52:55 -0600

    Allow api filtering by specifying a query parameter for the category, goal, or sequence id; Close MVP-33

--- tndata_backend/goals/api.py
+++ tndata_backend/goals/api.py
@@ -20,6 +20,13 @@ class GoalViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Goal.objects.all()
     serializer_class = serializers.GoalSerializer
 
+    def get_queryset(self):
+        if 'category' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                categories__id=self.request.GET['category']
+            )
+        return self.queryset
+
 
 class TriggerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Trigger.objects.all()
@@ -30,7 +37,33 @@ class BehaviorSequenceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.BehaviorSequence.objects.all()
     serializer_class = serializers.BehaviorSequenceSerializer
 
+    def get_queryset(self):
+        if 'category' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                categories__id=self.request.GET['category']
+            )
+        if 'goal' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                goals__id=self.request.GET['goal']
+            )
+        return self.queryset
+
 
 class BehaviorActionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.BehaviorAction.objects.all()
     serializer_class = serializers.BehaviorActionSerializer
+
+    def get_queryset(self):
+        if 'category' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                sequence__categories__id=self.request.GET['category']
+            )
+        if 'goal' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                sequence__goals__id=self.request.GET['goal']
+            )
+        if 'sequence' in self.request.GET:
+            self.queryset = self.queryset.filter(
+                sequence__id=self.request.GET['sequence']
+            )
+        return self.queryset
