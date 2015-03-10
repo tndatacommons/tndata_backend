13e1d48758ee06ebb35d694d73ac8b7bc3e469a8
Brad Montgomery <brad@bradmontgomery.net>
2015-03-10 17:11:39 -0500

    adding more info to the UserSerializer

--- tndata_backend/userprofile/serializers.py
+++ tndata_backend/userprofile/serializers.py
@@ -5,9 +5,15 @@ from . import models
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name =  serializers.Field(source='get_full_name')
+    userprofile_id =  serializers.Field(source='userprofile.id')
+
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'is_staff')
+        fields = (
+            'id', 'username', 'email', 'is_staff', 'first_name', 'last_name',
+            'full_name', 'date_joined', 'userprofile_id',
+        )
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
