651361cd443ea6c61a45e3090b3f0044f2826979
Brad Montgomery <brad@bradmontgomery.net>
2015-03-11 17:13:26 -0500

    documentation ftw

--- tndata_backend/userprofile/api.py
+++ tndata_backend/userprofile/api.py
@@ -3,21 +3,66 @@ from rest_framework import status, viewsets
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.decorators import detail_route
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from . import models
 from . import permissions
 from . import serializers
 
-# TODOs:
-#
-# 2. Exclude the POST forms for UserViewSet and UserProfileViewSet
-#    from the browseable api.
+# TODO: Exclude the POST forms for UserViewSet and UserProfileViewSet
+# from the browseable api (Ensure appropriate permissions for (un)authed users)
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """This endpoint defines methods that allow you to view, create, and update
+    User accounts.
+
+    ## Creating a User
+
+    POST to `/api/users/` with the following information:
+
+        {
+            "username": "YOUR-USERNAME",
+            "password": "YOUR-PASSWORD",
+            "email": "YOUR-EMAIL",
+            "first_name": "First",
+            "last_name": "Last"
+        }
+
+    The response includes the created user's info, such as their database id and
+    the id for their created User Profile *as well as* an Auth Token for subsequent
+    API requests.
+
+    ## Acquiring an Auth token
+
+    POST to `/api/auth/token/` with a `username` and `password`. The response
+    will contain a `token` attribute, which you can then include with subsequent
+    requests. Include the token in an `Authorization` HTTP header, where the
+    token is prefixed by the string, "Token", e.g.:
+
+        Authorization: Token <token-value-here>
+
+    You can use curl to test authenticated reqeusts, e.g.:
+
+        curl -X GET http://app.tndata.org/api/users/ -H 'Authorization: Token <YOUR-TOKEN>'
+
+    ## Retrieving a User's Info
+
+    Send a GET request to `/api/users/` or to `/api/users/{id}/`.
+    This should include a single result set that contains the authenticated user.
+
+    ## Setting a Password
+
+    Send a PUT request to `/api/users/{id}/` including at least the following:
+
+        {'username': <their username>, 'password': <new password>}
+
+    ## Options
+
+    See the *Options* on this page for more information regarding which fields
+    are required (during PUT requests).
+
+    """
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = get_user_model().objects.all()
     serializer_class = serializers.UserSerializer
@@ -37,8 +82,28 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """
-    This is a User Profile.
+    """This defines methods for viewing a User Profile. User Profiles are
+    created automatically after a user account is created.
+
+    This endpoint requires authorization, and requests must include an Auth Token.
+
+    ## Retrieving a User Profile
+
+    Send a GET request to `/api/userprofiles/` or to
+    `/api/userprofiles/{profile-id}/`. A User's profile id is available as
+    part of their [user data](/api/users/). This should include a single
+    result set that contains the authenticated user.
+
+    ## Updating a User Profile
+
+    Send a PUT request to `/api/userprofiles/{profile-id}/` including the data
+    that you wish to update.
+
+    ## Options
+
+    View the *Options* on this page for more details regarding which fields
+    are required.
+
     """
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = models.UserProfile.objects.all()
