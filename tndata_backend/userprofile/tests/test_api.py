0c852c431ab0d3de14e6b73450460eb4e2118101
Brad Montgomery <brad@bradmontgomery.net>
2015-03-13 19:14:16 -0500

    tests for userprofile viewsets

--- tndata_backend/userprofile/tests/test_api.py
+++ tndata_backend/userprofile/tests/test_api.py
@@ -66,3 +66,71 @@ class TestUsersAPI(APITestCase):
         data = {'username': 'me', 'password': 'secret'}
         response = self.client.post(url, data)
         self.assertEqual(response.data['token'], self.user.auth_token.key)
+
+
+class TestUserProfilesAPI(APITestCase):
+
+    def setUp(self):
+        self.User = get_user_model()
+        self.user = self.User.objects.create_user(
+            username="me",
+            email="me@example.com",
+            password="secret"
+        )
+        self.p = self.user.userprofile
+
+    def tearDown(self):
+        self.User.objects.all().delete()
+
+    def test_get_userprofile_list(self):
+        """Ensure unauthenticated requests don't return any data."""
+        url = reverse('userprofile-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 0)
+
+    #def test_post_userprofile_list_unauthorized(self):
+        #"""POSTing to the userprofile-list should not be allowed."""
+        #url = reverse('userprofile-list')
+        #response = self.client.post(url, {})
+        #self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_userprofile_detail_unauthorized(self):
+        """Ensure unauthenticated users cannot access user detail data."""
+        url = reverse('userprofile-detail', args=[self.p.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_userprofile_detail(self):
+        """Ensure authenticated users can access their data."""
+        url = reverse('userprofile-detail', args=[self.p.id])
+        # Include an appropriate `Authorization:` header on all requests.
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['home_zip'], '')  # was never set
+
+    # TODO: POST cuases a db error; restrict it.
+    # see: http://stackoverflow.com/questions/23639113/disable-a-method-in-a-viewset-django-rest-framework
+    #def test_post_userprofile_detail_not_allowed(self):
+        #"""Ensure we cannot post to userprofile-detail"""
+        #url = reverse('userprofile-detail', args=[self.p.id])
+        #response = self.client.post(url, {'home_zip': '12345'})
+        #self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_userprofile_detail_unauthorized(self):
+        """Ensure unauthenticated users cannot update data."""
+        url = reverse('userprofile-detail', args=[self.p.id])
+        response = self.client.put(url, {'home_zip': '12345'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # TODO
+    #def test_put_userprofile_detail(self):
+        #"""Ensure authenticated users can update their data."""
+        #url = reverse('userprofile-detail', args=[self.p.id])
+        ## Include an appropriate `Authorization:` header on all requests.
+        #self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key)
+        #response = self.client.post(url, {'home_zip': '12345'})
+
+        #self.assertEqual(response.status_code, status.HTTP_200_OK)
+        #self.assertEqual(response.data['home_zip'], '12345')  # updated!
