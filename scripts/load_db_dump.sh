61abb797ac885ce2b366cebd47a30063a7d2b101
Brad Montgomery <brad@bradmontgomery.net>
2015-03-17 18:11:43 -0500

    update script to reload DB from a dump

--- scripts/load_db_dump.sh
+++ scripts/load_db_dump.sh
@@ -2,9 +2,11 @@
 
 # IF there's a `dump.sql` file in the same directory, just load it up!
 # so get rid of that.
-if [ -e "dump.sql" ]
+if [ -e "/vagrant/dump.sql" ]
     then
-    psql -h 127.0.0.1 -U tndata_backend -d tndata_backend -f dump.sql
+    sudo -u postgres dropdb tndata_backend;
+    sudo -u postgres createdb -E UTF8 -T template0 tndata_backend
+    sudo -u postgres psql -h 127.0.0.1 -U tndata_backend -d tndata_backend -f /vagrant/dump.sql
 else
     echo
     echo "Could not find a DB dump.sql file!"
