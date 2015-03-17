6eab325e5ff71dc849b4000d810e377783c06fff
Brad Montgomery <brad@bradmontgomery.net>
2015-03-17 13:36:11 -0500

    more tests for the user/userprofile apis

--- tndata_backend/userprofile/tests/test_api.py
+++ tndata_backend/userprofile/tests/test_api.py
@@ -6,6 +6,8 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from .. models import UserProfile
+
 
 class TestUsersAPI(APITestCase):
 
@@ -129,6 +131,7 @@ class TestUserProfilesAPI(APITestCase):
 
     def tearDown(self):
         self.User.objects.all().delete()
+        UserProfile.objects.all().delete()
 
     def test_get_userprofile_list(self):
         """Ensure unauthenticated requests don't return any data."""
@@ -137,11 +140,18 @@ class TestUserProfilesAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 0)
 
-    #def test_post_userprofile_list_unauthorized(self):
-        #"""POSTing to the userprofile-list should not be allowed."""
-        #url = reverse('userprofile-list')
-        #response = self.client.post(url, {})
-        #self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_post_userprofile_list_not_allowed(self):
+        """POSTing to the userprofile-list should not be allowed."""
+        url = reverse('userprofile-list')
+        response = self.client.post(url, {})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # Not allowed Even when authorized.
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key
+        )
+        response = self.client.post(url, {})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_get_userprofile_detail_unauthorized(self):
         """Ensure unauthenticated users cannot access user detail data."""
@@ -158,13 +168,18 @@ class TestUserProfilesAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['home_zip'], '')  # was never set
 
-    # TODO: POST cuases a db error; restrict it.
-    # see: http://stackoverflow.com/questions/23639113/disable-a-method-in-a-viewset-django-rest-framework
-    #def test_post_userprofile_detail_not_allowed(self):
-        #"""Ensure we cannot post to userprofile-detail"""
-        #url = reverse('userprofile-detail', args=[self.p.id])
-        #response = self.client.post(url, {'home_zip': '12345'})
-        #self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_post_userprofile_detail_not_allowed(self):
+        """Ensure we cannot post to userprofile-detail"""
+        url = reverse('userprofile-detail', args=[self.p.id])
+        response = self.client.post(url, {'home_zip': '12345'})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # Even when authorized
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key
+        )
+        response = self.client.post(url, {'home_zip': '12345'})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_put_userprofile_detail_unauthorized(self):
         """Ensure unauthenticated users cannot update data."""
@@ -172,13 +187,15 @@ class TestUserProfilesAPI(APITestCase):
         response = self.client.put(url, {'home_zip': '12345'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # TODO
-    #def test_put_userprofile_detail(self):
-        #"""Ensure authenticated users can update their data."""
-        #url = reverse('userprofile-detail', args=[self.p.id])
-        ## Include an appropriate `Authorization:` header on all requests.
-        #self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key)
-        #response = self.client.post(url, {'home_zip': '12345'})
-
-        #self.assertEqual(response.status_code, status.HTTP_200_OK)
-        #self.assertEqual(response.data['home_zip'], '12345')  # updated!
+    def test_put_userprofile_detail_authorized(self):
+        """Ensure authenticated users can update their data."""
+        url = reverse('userprofile-detail', args=[self.p.id])
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.user.auth_token.key
+        )
+        response = self.client.put(url, {'birthdate': '1900-01-31'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data['birthdate'].strftime("%Y-%m-%d"),
+            '1900-01-31'
+        )
