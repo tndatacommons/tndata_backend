24349c7754894d7ad7ea2b19dff4f26818d3cee0
Brad Montgomery <brad@bradmontgomery.net>
2015-04-08 14:05:44 -0500

    adding migrations for an old cahnge to LIkertQuestions

---
+++ tndata_backend/survey/migrations/0016_auto_20150408_1402.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('survey', '0015_auto_20150325_1306'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='likertquestion',
+            name='scale',
+            field=models.CharField(default='5_point_agreement', help_text='Select the Scale for this question', choices=[('7_point_agreement', '7 Point Agreement'), ('5_point_satisfaction', '5 Point Satisfaction'), ('9_point_agreement', '9 Point Agreement'), ('5_point_frequency', '5 Point Frequency'), ('7_point_importance', '7 Point Importance'), ('9_point_reverse_agreement', '9 Point Reverse Agreement'), ('5_point_reverse_agreement', '5 Point Reverse Agreement'), ('7_point_frequency', '7 Point Frequency'), ('7_point_reverse_agreement', '7 Point Reverse Agreement'), ('7_point_satisfaction', '7 Point Satisfaction'), ('5_point_agreement', '5 Point Agreement')], max_length=32),
+            preserve_default=True,
+        ),
+    ]
