077ea08a5014421960b819c797f76a35a8799fa8
Brad Montgomery <brad@bradmontgomery.net>
2015-04-21 17:33:00 -0500

    removing djorm_pgarray and upgrading to the most recent django_debug_toolbar

--- tndata_backend/tndata_backend/settings/base.py
+++ tndata_backend/tndata_backend/settings/base.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'djorm_pgarray',
     'jsonfield',
     'rest_framework',
     'rest_framework.authtoken',
