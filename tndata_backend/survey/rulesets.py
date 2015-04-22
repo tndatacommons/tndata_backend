8afe1bd1730d79a6f9c5db6fe7ce68cb8ca308e4
Brad Montgomery <brad@bradmontgomery.net>
2015-04-22 12:46:31 -0500

    added a function to register rulesets from withing survey.rulesets

--- tndata_backend/survey/rulesets.py
+++ tndata_backend/survey/rulesets.py
@@ -2,18 +2,24 @@ from business_rules.actions import BaseActions, rule_action
 from business_rules.variables import BaseVariables
 from business_rules.variables import (
     boolean_rule_variable,
-    numeric_rule_variable,
     string_rule_variable
 )
-from rules.rulesets import ModelRuleset
-from . models import BinaryResponse
+from rules.rulesets import ModelRuleset, ruleset
+from . import models
+
 
 # NOTE: After createing a ModelRulset subclass, you need to register it in the
 # AppConfig.ready method.
+# TODO: I'm not sure where to call this, so It's currently not getting called
+# anywhere. We can't do this in the AppConfig.ready, because this will load
+# Models before the app is actually ready.
+
+def register_rules(app_name):
+    """Register all of the Rules."""
+    ruleset.register(app_name, BinaryResponseRuleset)
+
 
 # TODO: Need aggregated data for survey responses before this is meaningful.
-
-
 class BinaryResponseVariables(BaseVariables):
     """Exposes a `BinaryResponse`'s data as business-rules variables."""
     def __init__(self, response):
@@ -42,7 +48,7 @@ class BinaryResponseActions(BaseActions):
 
 
 class BinaryResponseRuleset(ModelRuleset):
-    model = BinaryResponse
+    model = models.BinaryResponse
     variables = BinaryResponseVariables
     actions = BinaryResponseActions
     stop_on_first_trigger = False
