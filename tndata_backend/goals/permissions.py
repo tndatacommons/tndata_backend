6b355ea50052b806db54e1092c999a1cc5266847
Brad Montgomery <brad@bradmontgomery.net>
2015-04-22 12:42:28 -0500

    moving the _get_group_and_permission function into utils so I can reuse it

--- tndata_backend/goals/permissions.py
+++ tndata_backend/goals/permissions.py
@@ -10,25 +10,11 @@ custom groups for people who create and review content:
 """
 # NOTE: Dont' import any models directly; the functions here are called from
 # a migration, so let's import from an AppConfig if at all possible.
+from utils.permissions import _get_group_and_permission
 
 # Group Names
 CONTENT_AUTHORS = "Content Authors"
 CONTENT_EDITORS = "Content Editors"
-
-
-def _get_group_and_permission(apps):
-    """Use the given AppConfig (if available) to import Group and Permission.
-    Returns a (Group, Permission) tuple.
-    """
-    if apps is not None:
-        return (
-            apps.get_model("auth", "Group"),
-            apps.get_model("auth", "Permission")
-        )
-    else:
-        # Just import as normal
-        from django.contrib.auth.models import Group, Permission
-        return (Group, Permission)
 
 
 def get_or_create_content_authors(apps=None, schema_editor=None):
