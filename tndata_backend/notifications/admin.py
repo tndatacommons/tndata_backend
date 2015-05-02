cc19d7385d5028ad6bca77cda8258327286949bb
Brad Montgomery <brad@bradmontgomery.net>
2015-05-02 14:48:32 -0500

    adding an admin action to send GCM messages

--- tndata_backend/notifications/admin.py
+++ tndata_backend/notifications/admin.py
@@ -8,5 +8,12 @@ class GCMMessageAdmin(admin.ModelAdmin):
     )
     list_filter = ('success', 'response_code')
     search_fields = ['message_id', 'registration_id']
+    actions = ['send_notification']
+
+    def send_notification(self, request, queryset):
+        for obj in queryset:
+            obj.send()
+    send_notification.short_description = "Send Message via GCM"
+
 
 admin.site.register(models.GCMMessage, GCMMessageAdmin)
