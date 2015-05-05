36acf9ff705b8274a283defbdcf2a66777e3dc04
Brad Montgomery <brad@bradmontgomery.net>
2015-05-05 09:29:25 -0500

    don't import any external stuff in a migration

--- tndata_backend/goals/migrations/0033_create_content_editor_and_author_groups.py
+++ tndata_backend/goals/migrations/0033_create_content_editor_and_author_groups.py
@@ -2,10 +2,46 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from ..permissions import (
-    get_or_create_content_authors,
-    get_or_create_content_editors,
-)
+from .. permissions import CONTENT_AUTHORS, CONTENT_EDITORS
+
+
+def _get_group_and_permission(apps):
+    return (
+        apps.get_model("auth", "Group"),
+        apps.get_model("auth", "Permission")
+    )
+
+
+def create_content_authors(apps, schema_editor=None):
+    Group, Permission = _get_group_and_permission(apps)
+    group, created = Group.objects.get_or_create(name=CONTENT_AUTHORS)
+    perms = [
+        "view_category",
+        "view_trigger",
+    ]
+    for obj in ['goal', 'behavior', 'action']:
+        perms.append("add_{0}".format(obj))
+        perms.append("change_{0}".format(obj))
+        perms.append("view_{0}".format(obj))
+    for p in Permission.objects.filter(codename__in=perms):
+        group.permissions.add(p)
+    group.save()
+
+
+def create_content_editors(apps, schema_editor=None):
+    Group, Permission = _get_group_and_permission(apps)
+    group, created = Group.objects.get_or_create(name=CONTENT_EDITORS)
+    perms = []
+    for obj in ['category', 'goal', 'behavior', 'action', 'trigger']:
+        perms.append("add_{0}".format(obj))
+        perms.append("change_{0}".format(obj))
+        perms.append("view_{0}".format(obj))
+        perms.append("delete_{0}".format(obj))
+        perms.append("publish_{0}".format(obj))
+        perms.append("decline_{0}".format(obj))
+    for p in Permission.objects.filter(codename__in=perms):
+        group.permissions.add(p)
+    group.save()
 
 
 class Migration(migrations.Migration):
@@ -16,6 +52,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(get_or_create_content_authors),
-        migrations.RunPython(get_or_create_content_editors),
+        migrations.RunPython(create_content_authors),
+        migrations.RunPython(create_content_editors),
     ]
