3a806cb079de980c22962c7d3de2f5c77a9c03cf
Brad Montgomery <brad@bradmontgomery.net>
2015-06-02 16:57:54 -0500

    oops. forgot an import

--- tndata_backend/goals/management/commands/aggregate_progress.py
+++ tndata_backend/goals/management/commands/aggregate_progress.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from goals.models import GoalProgress
+from goals.models import GoalProgress, CategoryProgress
 
 
 class Command(BaseCommand):
