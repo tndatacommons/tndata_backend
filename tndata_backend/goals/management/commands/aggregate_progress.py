2ad4d119a1aeb6b0247b754f1275281d74bdac00
Brad Montgomery <brad@bradmontgomery.net>
2015-06-02 14:43:46 -0500

    updating the management command to generate CategoryProgress objects (and renaming it)

--- tndata_backend/goals/management/commands/aggregate_progress.py
+++ tndata_backend/goals/management/commands/aggregate_progress.py
@@ -4,9 +4,14 @@ from goals.models import GoalProgress
 
 
 class Command(BaseCommand):
-    help = 'Creates GoalProgress objects. Run Nightly'
+    help = 'Creates GoalProgress and CategoryProgress objects. Run Nightly'
 
     def handle(self, *args, **options):
         User = get_user_model()
+        # Generate scores for progress toward goals...
         for user in User.objects.filter(usergoal__isnull=False).distinct():
             GoalProgress.objects.generate_scores(user)
+
+        # ...and toward categories.
+        for user in User.objects.filter(usercategory__isnull=False).distinct():
+            CategoryProgress.objects.generate_scores(user)
