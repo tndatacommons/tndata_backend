383ca0498d3d133c78b1bd7ac4735b2ec17cadbd
Brad Montgomery <brad@bradmontgomery.net>
2015-07-13 15:39:17 -0500

    include TEMPLATES settings for django 1.8

--- tndata_backend/tndata_backend/settings/base.py
+++ tndata_backend/tndata_backend/settings/base.py
@@ -28,8 +28,31 @@ ALLOWED_HOSTS = [
     'localhost', '127.0.0.1', '.tndata.org', '.tndata.org.', '104.236.244.232',
 ]
 
-TEMPLATE_DIRS = [
-    os.path.join(BASE_DIR, 'templates')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'OPTIONS': {
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+            'context_processors': (
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.tz",
+            ),
+        },
+    },
 ]
 
 
