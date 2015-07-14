b95f004079ef9a9d1b96e040bc1e5268cda7871e
Brad Montgomery <brad@bradmontgomery.net>
2015-07-14 11:46:57 -0500

    adding tndata.ngrok.io to allowed hosts

--- tndata_backend/tndata_backend/settings/base.py
+++ tndata_backend/tndata_backend/settings/base.py
@@ -27,7 +27,7 @@ TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [
     'localhost', '127.0.0.1',
     '.tndata.org', '.tndata.org.', '104.236.244.232',
-    'brad.ngrok.io',
+    'brad.ngrok.io', 'tndata.ngrok.io',
 ]
 
 TEMPLATES = [
