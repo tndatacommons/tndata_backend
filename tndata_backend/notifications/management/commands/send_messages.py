e7cf86d6b8d06af9996d7c0b41903b695e3efe53
Brad Montgomery <brad@bradmontgomery.net>
2015-07-27 10:58:35 -0500

    ping certain people via PM on slack when their messages are sent to GCM

--- tndata_backend/notifications/management/commands/send_messages.py
+++ tndata_backend/notifications/management/commands/send_messages.py
@@ -2,8 +2,17 @@ import logging
 
 from django.core.management.base import BaseCommand
 from notifications.models import GCMMessage
+from utils import slack
+
 
 logger = logging.getLogger("loggly_logs")
+
+# user.email -> slack username
+slack_users = {
+    'brad@brad.tips': 'bkmontgomery',
+    'ringram@tndata.org': 'ringram',
+    'ismaha91@gmail.com': 'ialonso',
+}
 
 
 class Command(BaseCommand):
@@ -20,6 +29,18 @@ class Command(BaseCommand):
             for message in messages:
                 try:
                     message.send()
+
+                    # --- Temporary debegging via Slack -----------------------
+                    # For some of our messages, we want to send a PM to the
+                    # user that their message was sent up to GCM.
+                    slack_user = slack_users.get(message.user.email)
+                    if slack_user is not None:
+                        slack_message = "Queued Notification on GCM: {0}".format(
+                            message.message
+                        )
+                        slack.post_private_message(slack_user, slack_message)
+                    # ---------------------------------------------------------
+
                 except Exception:
                     log_msg = "Failed to send GCMMEssage id = {0}".format(message.id)
                     logger.error(log_msg)
