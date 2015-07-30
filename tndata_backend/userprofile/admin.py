0a30b3c1468f5dfae971595e7aadc25432231000
Brad Montgomery <brad@bradmontgomery.net>
2015-07-30 10:58:42 -0500

    updating admin for userprofile

--- tndata_backend/userprofile/admin.py
+++ tndata_backend/userprofile/admin.py
@@ -6,9 +6,11 @@ from utils.admin import UserRelatedModelAdmin
 
 class UserProfileAdmin(UserRelatedModelAdmin):
     list_display = (
-        'user', 'user_email', 'user_first', 'user_last',
+        'user', 'user_email', 'user_first', 'user_last', 'timezone',
+        'needs_onboarding'
     )
     search_fields = (
         'user__username', 'user__email', 'user__first_name', 'user__last_name',
     )
+
 admin.site.register(models.UserProfile, UserProfileAdmin)
