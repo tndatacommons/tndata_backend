0309a289b4b1add2679d4695e9a220bb9cb1f093
Brad Montgomery <brad@bradmontgomery.net>
2015-08-07 15:07:07 -0500

    conditional import of clog; and a debug_user_data that prints the hierarchy of user-selected content

--- tndata_backend/goals/utils.py
+++ tndata_backend/goals/utils.py
@@ -1,5 +1,14 @@
 import csv
 from io import TextIOWrapper
+from django.conf import settings
+
+
+# Import clog if we're in debug otherwise make it a noop
+if settings.DEBUG:
+    from clog import clog
+else:
+    def clog(*args, **kwargs):
+        pass
 
 
 def read_uploaded_csv(uploaded_file, encoding='utf-8', errors='ignore'):
@@ -25,3 +34,66 @@ def read_uploaded_csv(uploaded_file, encoding='utf-8', errors='ignore'):
     for row in csv.reader(file):
         if any(row):
             yield row
+
+
+def debug_user_data(user):
+    """Given a user, print out their selected hierarchy of content."""
+
+    data = {'orphaned': {'goals': [], 'actions': [], 'behaviors': []}}
+
+    for uc in user.usercategory_set.all():
+        data[uc.category.title] = {}
+
+    for ug in user.usergoal_set.all():
+        added = False
+        for cat in ug.goal.categories.all():
+            try:
+                data[cat.title][ug.goal.title] = {}
+                added = True
+            except KeyError:
+                pass
+
+        if not added:
+            data['orphaned']['goals'].append(ug.goal.title)
+
+    for ub in user.userbehavior_set.all():
+        added = False
+        for goal in ub.behavior.goals.all():
+            for cat in goal.categories.all():
+                try:
+                    data[cat.title][goal.title][ub.behavior.title] = []
+                    added = True
+                except KeyError:
+                    pass
+        if not added:
+            data['orphaned']['behaviors'].append(ug.behavior.title)
+
+    for ua in user.useraction_set.all():
+        behavior = ua.action.behavior
+        action = ua.action
+        added = False
+        for goal in behavior.goals.all():
+            for cat in goal.categories.all():
+                try:
+                    data[cat.title][goal.title][behavior.title].append(action.title)
+                    added = True
+                except KeyError:
+                    pass
+
+        if not added:
+            data['orphaned']['actions'].append(action.title)
+
+    # Better representation.
+    orphans = data.pop("orphaned")
+    for cat, goals in data.items():
+        print(cat)
+        for goal, behaviors in goals.items():
+            print("- {0}".format(goal))
+            for behavior, actions in behaviors.items():
+                print("-- {0}".format(behavior))
+                for a in actions:
+                    print("--- {0}".format(a))
+
+    print("\nOrphaned data")
+    for t, items in orphans.items():
+        print("{0}: {1}".format(t, ", ".join(items)))
