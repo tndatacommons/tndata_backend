eaf619c9d02352510ed5ae93e68b03071833981b
Brad Montgomery <brad@bradmontgomery.net>
2015-08-18 18:02:22 -0500

    ensure we get a unique id for the markdown popup

--- tndata_backend/utils/widgets.py
+++ tndata_backend/utils/widgets.py
@@ -1,6 +1,9 @@
+import uuid
+
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.forms import Media, Textarea
 from django.forms.utils import flatatt
+from django.template.defaultfilters import slugify
 from django.utils.encoding import force_text
 from django.utils.html import format_html
 
@@ -9,6 +12,10 @@ class TextareaWithMarkdownHelperWidget(Textarea):
     """TextArea widget with a markdown guide beneath it.
 
     """
+    def _guide_id(self):
+        """Generate a unique id for this widget's markdown link"""
+        return slugify(uuid.uuid4())
+
     def _mardown_guide(self):
         """quick and dirty hack to propuate a Foundation dropdown with some
         mardown authoring guides."""
@@ -50,18 +57,19 @@ Links:
         """
 
     def _get_markup(self):
+        guide_id = self._guide_id()
         return (
             '<div><textarea{}>\r\n{}</textarea>'
             '<div class="md-support">'
             '  <strong>*bold*</strong>, <em>_italics_</em>, '
             '  1. numbered list, * bullets'
             '<span class="pull-right">'
-            '<a data-dropdown="md-guide" aria-controls="md-guide" '
+            '<a data-dropdown="' + guide_id + '" aria-controls="md-guide" '
             '   aria-expanded="false" data-options="align:top" '
             '   class="md-guide">Markdown</a>'
-            '<div id="md-guide" data-dropdown-content '
-            '     class="medium f-dropdown content" aria-hidden="true" tabindex="-1">'
-            + self._mardown_guide() +
+            '<div id="' + guide_id + '" data-dropdown-content '
+            '     class="medium f-dropdown content" aria-hidden="true" '
+            '     tabindex="-1">' + self._mardown_guide() +
             '</div></span></div></div>'
         )
 
