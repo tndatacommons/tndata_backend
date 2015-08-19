f5f31fba1c667ca17167df014c80ef1394102ccf
Brad Montgomery <brad@bradmontgomery.net>
2015-08-19 12:59:29 -0500

    initial, simple tests for the goals 'create_notifications' command

---
+++ tndata_backend/goals/tests/test_managementcommands.py
@@ -0,0 +1,73 @@
+from unittest.mock import patch
+
+from datetime import time
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+
+from .. models import (
+    Action,
+    Behavior,
+    Trigger,
+    UserAction,
+    UserBehavior,
+)
+
+
+class TestCreateNotifications(TestCase):
+    """Tests for the `create_notifications` management command."""
+
+    def test_create_notificaitons_no_content(self):
+        log_path = "goals.management.commands.create_notifications.logger"
+        with patch(log_path) as logger:
+            call_command('create_notifications')
+
+            # We should have logged a 'finished' message
+            logger.info.assert_called_with("Created 0 notification messages.")
+
+    def test_create_notifications_with_content(self):
+        User = get_user_model()
+        user = User.objects.create_user('x', 'x@example.com', 'pass')
+
+        # Ensure the user has a device
+        user.gcmdevice_set.create(registration_id="REGID", device_name="test")
+
+        # Some test content
+        default_trigger = Trigger.objects.create(
+            name="default",
+            trigger_type="time",
+            time=time(12, 34),
+            recurrences="RRULE:FREQ=DAILY"
+        )
+        behavior = Behavior.objects.create(title='B', state="published")
+        action1 = Action.objects.create(
+            behavior=behavior,
+            title="A1",
+            state="published",
+            default_trigger=default_trigger,
+        )
+        action2 = Action.objects.create(
+            behavior=behavior, title="A2", state="published"
+        )
+
+        custom_trigger = Trigger.objects.create(
+            user=user,
+            name="test custom trigger",
+            trigger_type="time",
+            time=time(9, 30),
+            recurrences="RRULE:FREQ=DAILY"
+        )
+        UserBehavior.objects.create(user=user, behavior=behavior)
+        UserAction.objects.create(user=user, action=action1)
+        UserAction.objects.create(
+            user=user,
+            action=action2,
+            custom_trigger=custom_trigger
+        )
+
+        log_path = "goals.management.commands.create_notifications.logger"
+        with patch(log_path) as logger:
+            call_command('create_notifications')
+
+            # We should have logged a 'finished' message
+            logger.info.assert_called_with("Created 3 notification messages.")
