8be2adea95f8da2588739932ccc319d6549a949e
Brad Montgomery <brad@bradmontgomery.net>
2015-08-26 12:14:16 -0500

    adding an iconbool filter

--- tndata_backend/utils/templatetags/util_filters.py
+++ tndata_backend/utils/templatetags/util_filters.py
@@ -19,3 +19,25 @@ def process_markdown(text):
 
     """
     return mark_safe(markdown.markdown(text))
+
+
+
+@register.filter("iconbool", is_safe=True)
+def fa_icons_for_booleans(value, invert=False):
+    """Given a boolean value, this filter outputs a font-awesome icon + the
+    word "Yes" or "No"
+
+    Usage:
+
+        {{ is_widget|iconbool }}
+
+    """
+    if bool(value) and invert:
+        result = '<i class="fa fa-ban"></i> No'
+    elif not bool(value) and invert:
+        result = '<i class="fa fa-check"></i> Yes'
+    elif bool(value):
+        result = '<i class="fa fa-check"></i> Yes'
+    else:
+        result = '<i class="fa fa-ban"></i> No'
+    return mark_safe(result)
