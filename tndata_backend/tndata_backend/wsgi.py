d0a37f3aab48e27ac0eb336a6541a62448d78193
Brad Montgomery <brad@bradmontgomery.net>
2015-09-23 09:51:48 -0500

    only set the DJANGO_SETTINGS_MODUL env var if it's not already set

--- tndata_backend/tndata_backend/wsgi.py
+++ tndata_backend/tndata_backend/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tndata_backend.settings")
+
+# Check to see if we have an environment variable defining the settings,
+# and if not, set a default.
+if os.environ.get("DJANGO_SETTINGS_MODULE") is None:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tndata_backend.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
