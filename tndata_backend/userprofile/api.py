7ef79d4c8f76e6211f70fe7bc2ab89181f9f37a0
Brad Montgomery <brad@bradmontgomery.net>
2015-10-01 16:31:03 -0500

    fixing var name

--- tndata_backend/userprofile/api.py
+++ tndata_backend/userprofile/api.py
@@ -175,7 +175,7 @@ class UserViewSet(viewsets.ModelViewSet):
     * `progress` -- an object containing the number of actions completed today,
       the number of total actions scheduled for today, and the percentage of
       those completed.
-    * `upcoming_actions` -- a list of the `UserAction`s that are relevant for
+    * `upcoming` -- a list of the `UserAction`s that are relevant for
       today (i.e. the user has a reminder scheduled for today)
     * `suggestions` -- a list of suggested `Goal`s for the user.
 
