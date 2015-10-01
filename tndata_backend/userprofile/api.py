974a969f0bde6e7885d06bc7e0b0299f723a5a6f
Brad Montgomery <brad@bradmontgomery.net>
2015-10-01 15:16:56 -0500

    sprucing up the docs

--- tndata_backend/userprofile/api.py
+++ tndata_backend/userprofile/api.py
@@ -132,9 +132,46 @@ class UserPlaceViewSet(mixins.CreateModelMixin,
 
 class UserViewSet(viewsets.ModelViewSet):
     """This endpoint defines methods that allow you to view, create, and update
-    User accounts.
+    User accounts. Additionally, it exposes several bits of user data, therefore
+    acting as a decently complete single point of information for a User.
 
-    ## Creating a User
+    Sections in this documentation:
+
+    * <a href="#user-data">User Data</a>
+    * <a href="#creating-a-user">Creating a User</a>
+    * <a href="#acquiring-an-auth-token">Acquiring an Auth token</a>
+    * <a href="#logging-out">Logging Out</a>
+    * <a href="#retrieving-a-users-info">Retrieveing a User's Info</a>
+    * <a href="#setting-a-password">Setting a Password</a>
+    * <a href="#options">Options</a>
+
+    ----
+
+    ## User Data
+
+    The following user data is available from this endpoint as an item in the
+    `results` array.
+
+    * `id` -- Unique database ID for the User.
+    * `username` -- The user's username. For users that sign up through the app,
+      the is a unique hash and is not human-friendly.
+    * `email` -- the user's email address.
+    * `is_staff` -- True or False: Indicates if users can log in to admin tools.
+    * `first_name` -- The user's first name.
+    * `last_name` -- The user's last name.
+    * `full_name` -- Combination of first and last name
+    * `timezone` -- The user's given timezone.
+    * `date_joined` -- Date the user joined.
+    * `userprofile_id` -- Unique ID for the [UserProfile](/api/userprofiles/)
+    * `token` -- The user's [auth token](#acquiring-an-autho-token)
+    * `needs_onboarding` -- Whether or not the user should go through onboarding.
+    * `places` -- An array of the [user's Places](/api/users/places/)
+    * `goals` -- An array of the user's selected [Goals](/api/users/goals/)
+    * `behaviors` -- An array of the user's selected [Behaviors](/api/users/behaviors/)
+    * `actions` -- An array of the user's selected [Actions](/api/users/actions/)
+    * `categories` -- An array of the user's [Categories](/api/users/categories/)
+
+    ## Creating a User <a href="#creating-a-user">&para;</a>
 
     POST to `/api/users/` with the following information:
 
@@ -160,7 +197,7 @@ class UserViewSet(viewsets.ModelViewSet):
     and the id for their created User Profile *as well as* an Auth Token for
     subsequent API requests.
 
-    ## Acquiring an Auth token
+    ## Acquiring an Auth token <a href="#acquiring-an-auth-token">&para;</a>
 
     POST to `/api/auth/token/` with either an  email/password pair:
 
@@ -177,25 +214,25 @@ class UserViewSet(viewsets.ModelViewSet):
 
         curl -X GET http://app.tndata.org/api/users/ -H 'Authorization: Token <YOUR-TOKEN>'
 
-    ## Logging out.
+    ## Logging out <a href="#logging-out">&para;</a>
 
     A client of the api can log out of the api by sending a POST request to
     `/api/auth/logout/`. Include additional details in that request to trigger
     side effects (e.g. POST a `{registration_id: 'YOUR-REGISTRATION-ID'}` payload
     to remove your device's GCM registration on logout.
 
-    ## Retrieving a User's Info
+    ## Retrieving a User's Info <a href="#retrieving-a-users-info">&para;</a>
 
     Send a GET request to `/api/users/` or to `/api/users/{id}/`.
     This should include a single result set that contains the authenticated user.
 
-    ## Setting a Password
+    ## Setting a Password <a href="#setting-a-password">&para;</a>
 
     Send a PUT request to `/api/users/{id}/` including at least the following:
 
         {'username': <their username>, 'password': <new password>}
 
-    ## Options
+    ## Options <a href="#options">&para;</a>
 
     See the *Options* on this page for more information regarding which fields
     are required (during PUT requests).
