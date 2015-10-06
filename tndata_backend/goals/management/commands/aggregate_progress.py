55148c294f67627b0402025d22814d769debd6d2
Brad Montgomery <brad@bradmontgomery.net>
2015-10-06 15:50:55 -0500

    updating aggregate_progress to re-calculate GoalProgress action stats

--- tndata_backend/goals/management/commands/aggregate_progress.py
+++ tndata_backend/goals/management/commands/aggregate_progress.py
@@ -39,6 +39,14 @@ class Command(BaseCommand):
             logger.error(err_msg.format("GoalProgress"), exc_info=1)
 
         try:
+            # Re-calculate action progress stats for GoalProgress objects over
+            # the history range in which we're interested.
+            dt = timezone.now() - timedelta(days=settings.PROGRESS_HISTORY_DAYS)
+            for gp in GoalProgress.objects.filter(reported_on__contains=dt.date()):
+                gp.save()  # Triggers the re-calculation
+        except Exception:
+            logger.error(err_msg.format("GoalProgress Action Stats"), exc_info=1)
+        try:
             # Generate scores for category progress for all users that have
             # selected one or more categories.
             for user in User.objects.filter(usercategory__isnull=False).distinct():
