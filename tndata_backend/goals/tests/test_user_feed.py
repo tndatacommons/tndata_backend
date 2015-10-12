2c792d80c4fdaaeb0db90b8774ca1d335b3a8b33
Brad Montgomery <brad@bradmontgomery.net>
2015-10-12 13:18:45 -0500

    more tests for the user feed

--- tndata_backend/goals/tests/test_user_feed.py
+++ tndata_backend/goals/tests/test_user_feed.py
@@ -1,8 +1,11 @@
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
+from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
+from django.core.management import call_command
 from django.test import TestCase
 from django.utils import timezone
-from unittest.mock import patch
+from model_mommy import mommy
 
 from .. models import (
     Action,
@@ -13,6 +16,7 @@ from .. models import (
     UserAction,
     UserBehavior,
     UserCategory,
+    UserCompletedAction,
     UserGoal,
 )
 from .. import user_feed
@@ -23,6 +27,248 @@ def tzdt(*args, **kwargs):
     tz = kwargs.pop("tz", timezone.utc)
     dt = datetime(*args)
     return timezone.make_aware(dt, timezone=tz)
+
+
+class TestUserProgress(TestCase):
+    """This test case sets up data for the user, and then proceeds to verify
+    their progress info. It focuses on the following functions:
+
+    * it runs the `aggregate_progress` management command
+    * user_feed.action_feedback - Today's feedback data for an action.
+    * user_feed.todays_actions_progress - The progress stats for today's
+      scheduled actions.
+
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.User = get_user_model()
+        cls.user = cls.User.objects.create_user('user', 'u@example.com', 'secret')
+
+        # Create a Category/Goal/Behavior/Action(s) content
+        cls.category = mommy.make(Category, title="CAT", state="published")
+        cls.goal = mommy.make(Goal, title="GTITLE", state='published')
+        cls.goal.categories.add(cls.category)
+        cls.behavior = mommy.make(Behavior, title="BEH", state='published')
+        cls.behavior.goals.add(cls.goal)
+        cls.action1 = mommy.make(
+            Action, title='A1', behavior=cls.behavior, state='published'
+        )
+        cls.action2 = mommy.make(
+            Action, title='A2', behavior=cls.behavior, state='published'
+        )
+        cls.action3 = mommy.make(
+            Action, title='A3', behavior=cls.behavior, state='published'
+        )
+
+        # Assign the user the above content
+        cls.uc = mommy.make(UserCategory, user=cls.user, category=cls.category)
+        cls.ug = mommy.make(UserGoal, user=cls.user, goal=cls.goal)
+        cls.ub = mommy.make(UserBehavior, user=cls.user, behavior=cls.behavior)
+
+        dt = timezone.now()
+        cls.dt = tzdt(dt.year, dt.month, dt.day, 9, 0)  # Today, 9am
+        with patch('goals.models.timezone.now') as mock_now:
+            # Current time: 9am
+            mock_now.return_value = cls.dt
+
+            # Custom Triggers for each UserAction
+            cls.t1 = Trigger.objects.create(
+                user=cls.user,
+                name="a1-trigger",
+                trigger_type="time",
+                time=time(22, 0),
+                recurrences="RRULE:FREQ=DAILY"
+            )
+            cls.t2 = Trigger.objects.create(
+                user=cls.user,
+                name="a2-trigger",
+                trigger_type="time",
+                time=time(22, 30),
+                recurrences="RRULE:FREQ=DAILY"
+            )
+            cls.t3 = Trigger.objects.create(
+                user=cls.user,
+                name="a3-trigger",
+                trigger_type="time",
+                time=time(23, 0),
+                recurrences="RRULE:FREQ=DAILY"
+            )
+
+            # Create the UserActions
+            cls.ua1 = UserAction.objects.create(
+                user=cls.user,
+                action=cls.action1,
+                custom_trigger=cls.t1
+            )
+            cls.ua2 = UserAction.objects.create(
+                user=cls.user,
+                action=cls.action2,
+                custom_trigger=cls.t2
+            )
+            cls.ua3 = UserAction.objects.create(
+                user=cls.user,
+                action=cls.action3,
+                custom_trigger=cls.t3
+            )
+
+    def test_aggregate_progress(self):
+        """Calls the `aggregate_progress management command`."""
+        timezone_path = 'goals.management.commands.aggregate_progress.timezone'
+        log_path = "goals.management.commands.create_notifications.logger"
+        with patch(log_path):
+            with patch(timezone_path) as mock_timezone:
+                mock_timezone.now.return_value = self.dt  # today, 9am
+                call_command('aggregate_progress')
+
+    def test_action_feedback_zero_percent(self):
+        # Create some UserCompletedAction history for a single Action (10 days)
+        with patch('goals.models.timezone.now') as mock_now:
+            params = {
+                'user': self.user,
+                'useraction': self.ua1,
+                'action': self.action1,
+                'state': 'dismissed',
+            }
+            for d in range(1, 11):  # 10 days
+                mock_now.reset_mock()
+                mock_now.return_value = self.dt - timedelta(days=d)
+                UserCompletedAction.objects.create(**params)
+
+        results = user_feed.action_feedback(self.user, self.ua1)
+        expected = {
+            'title': "I've done some work to gtitle this month!",
+            'subtitle': 'Even small steps can help me reach my goal',
+            'total': 10,
+            'completed': 0,
+            'incomplete': 10,
+            'percentage': 0,
+        }
+        self.assertEqual(results, expected)
+
+    def test_action_feedback_half_completed(self):
+        # Create some UserCompletedAction history for a single Action (10 days)
+        with patch('goals.models.timezone.now') as mock_now:
+            params = {
+                'user': self.user,
+                'useraction': self.ua1,
+                'action': self.action1,
+                'state': 'completed',
+            }
+            for d in range(1, 11):  # 10 days
+                if d % 2 == 0:
+                    params['state'] = 'dismissed'
+                else:
+                    params['state'] = 'completed'
+                mock_now.reset_mock()
+                mock_now.return_value = self.dt - timedelta(days=d)
+                UserCompletedAction.objects.create(**params)
+
+        results = user_feed.action_feedback(self.user, self.ua1)
+        expected = {
+            'title': "I've done 5 activities to gtitle this month!",
+            'subtitle': 'I must really want this!',
+            'total': 10,
+            'completed': 5,
+            'incomplete': 5,
+            'percentage': 50,
+        }
+        self.assertEqual(results, expected)
+
+    def test_action_feedback_sixty_percent(self):
+        # Create some UserCompletedAction history for a single Action (10 days)
+        with patch('goals.models.timezone.now') as mock_now:
+            params = {
+                'user': self.user,
+                'useraction': self.ua1,
+                'action': self.action1,
+                'state': 'completed',
+            }
+            for d in range(1, 11):  # 10 days
+                if d > 6:
+                    params['state'] = 'dismissed'
+                mock_now.reset_mock()
+                mock_now.return_value = self.dt - timedelta(days=d)
+                UserCompletedAction.objects.create(**params)
+
+        results = user_feed.action_feedback(self.user, self.ua1)
+        expected = {
+            'title': "I've done 6 out of 10 activities to gtitle this month!",
+            'subtitle': "I'm doing great! I'll schedule another activity!",
+            'total': 10,
+            'completed': 6,
+            'incomplete': 4,
+            'percentage': 60,
+        }
+        self.assertEqual(results, expected)
+
+    def test_todays_actions_progress(self):
+        with patch('goals.models.timezone.now') as mock_now:
+            args = (self.dt.year, self.dt.month, self.dt.day, 9, 0)
+            mock_now.return_value = tzdt(*args)
+            params = {
+                'user': self.user,
+                'useraction': self.ua1,
+                'action': self.action1,
+                'state': 'completed',
+            }
+            # User has completed 2/3 of the actions.
+            UserCompletedAction.objects.create(**params)
+            params['useraction'] = self.ua2
+            params['action'] = self.action2
+            UserCompletedAction.objects.create(**params)
+            params['useraction'] = self.ua3
+            params['action'] = self.action3
+            params['state'] = 'dismissed'
+            UserCompletedAction.objects.create(**params)
+
+        with patch('utils.user_utils.timezone') as mock_tz:
+            # At 9am
+            args = (self.dt.year, self.dt.month, self.dt.day, 9, 0)
+            mock_tz.now.return_value = tzdt(*args)
+            mock_tz.make_aware = timezone.make_aware
+            mock_tz.make_naive = timezone.make_naive
+            mock_tz.utc = timezone.utc
+
+            progress = user_feed.todays_actions_progress(self.user)
+            expected = {
+                'completed': 2,
+                'total': 3,
+                'progress': 66,
+            }
+            self.assertEqual(progress, expected)
+
+            # At 10pm
+            mock_tz.reset_mock()
+            args = (self.dt.year, self.dt.month, self.dt.day, 22, 0)
+            mock_tz.now.return_value = tzdt(*args)
+            mock_tz.make_aware = timezone.make_aware
+            mock_tz.make_naive = timezone.make_naive
+            mock_tz.utc = timezone.utc
+
+            progress = user_feed.todays_actions_progress(self.user)
+            expected = {
+                'completed': 2,
+                'total': 3,
+                'progress': 66,
+            }
+            self.assertEqual(progress, expected)
+
+            # At 11:45pm
+            mock_tz.reset_mock()
+            args = (self.dt.year, self.dt.month, self.dt.day, 23, 45)
+            mock_tz.now.return_value = tzdt(*args)
+            mock_tz.make_aware = timezone.make_aware
+            mock_tz.make_naive = timezone.make_naive
+            mock_tz.utc = timezone.utc
+
+            progress = user_feed.todays_actions_progress(self.user)
+            expected = {
+                'completed': 2,
+                'total': 3,
+                'progress': 66,
+            }
+            self.assertEqual(progress, expected)
 
 
 class TestUserFeed(TestCase):
