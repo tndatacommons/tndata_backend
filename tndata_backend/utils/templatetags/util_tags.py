ebabba60923e1d2cfd9cc2288f6efbb3f19b65ea
Brad Montgomery <brad@bradmontgomery.net>
2015-10-19 13:27:40 -0500

    a rancom color template tag

--- tndata_backend/utils/templatetags/util_tags.py
+++ tndata_backend/utils/templatetags/util_tags.py
@@ -1,4 +1,6 @@
+import random
 from django import template
+
 register = template.Library()
 
 
@@ -21,7 +23,7 @@ def object_controls(context, obj, app_label, label=None):
     if label is None:
         label = model_name
 
-    perms = context.get('perms',[])  # Grab the permissions from the context.
+    perms = context.get('perms', [])  # Grab the permissions from the context.
 
     # To duplicate, a user must be able to create an object and the object
     # must have the appropriate api.
@@ -35,3 +37,48 @@ def object_controls(context, obj, app_label, label=None):
         'can_delete': delete_perm in perms,
         'can_duplicate': can_duplicate,
     }
+
+
+# pulled a list of colors from: http://www.computerhope.com/htmcolor.htm
+blue_colors = [
+    '#357EC7',  # Slate Blue
+    '#368BC1',  # Glacial Blue Ice
+    '#488AC7',  # Silk Blue
+    '#3090C7',  # Blue Ivy
+    '#659EC7',  # Blue Koi
+    '#87AFC7',  # Columbia Blue
+    '#95B9C7',  # Baby Blue
+    '#728FCE',  # Light Steel Blue
+    '#2B65EC',  # Ocean Blue
+    '#306EFF',  # Blue Ribbon
+    '#157DEC',  # Blue Dress
+    '#1589FF',  # Dodger Blue
+    '#6495ED',  # Cornflower Blue
+    '#6698FF',  # Sky Blue
+    '#38ACEC',  # Butterfly Blue
+    '#56A5EC',  # Iceberg
+    '#5CB3FF',  # Crystal Blue
+    '#3BB9FF',  # Deep Sky Blue
+    '#79BAEC',  # Denim Blue
+    '#82CAFA',  # Light Sky Blue
+    '#82CAFF',  # Day Sky Blue
+    '#A0CFEC',  # Jeans Blue
+    '#B7CEEC',  # Blue Angel
+    '#B4CFEC',  # Pastel Blue
+    '#C2DFFF',  # Sea Blue
+    '#C6DEFF',  # Powder Blue
+    '#AFDCEC',  # Coral Blue
+    '#ADDFFF',  # Light Blue
+    '#BDEDFF',  # Robin Egg Blue
+    '#CFECEC',  # Pale Blue Lily
+    '#E0FFFF',  # Light Cyan
+    '#EBF4FA',  # Water
+    '#F0F8FF',  # AliceBlue
+    '#F0FFFF',  # Azure
+    '#CCFFFF',  # Light Slate
+]
+
+
+@register.simple_tag
+def random_color():
+    return random.choice(blue_colors)
