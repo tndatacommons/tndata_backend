10dd2f7651437f5b7a27a9eaff7f611a19f5c08c
Brad Montgomery <brad@bradmontgomery.net>
2015-12-04 15:07:19 -0600

    removing the old diary app

--- tndata_backend/diary/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-class EntryAdmin(admin.ModelAdmin):
-    list_display = ('user', 'rank', 'submitted_on')
-
-
-admin.site.register(models.Entry, EntryAdmin)
