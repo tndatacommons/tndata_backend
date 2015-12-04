46e7af5ee059f51c96b1550fc98eb053036c50d0
Brad Montgomery <brad@bradmontgomery.net>
2015-12-04 15:14:28 -0600

    minor changes to script docs

--- scripts/load_db_dump.sh
+++ scripts/load_db_dump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is meant to be run from within a vagrant VM.
+#
 # IF there's a `dump.sql` file in the same directory, just load it up!
 # so get rid of that.
 if [ -e "/vagrant/dump.sql" ]
@@ -9,6 +11,6 @@ if [ -e "/vagrant/dump.sql" ]
     sudo -u postgres psql -h 127.0.0.1 -U tndata_backend -d tndata_backend -f /vagrant/dump.sql
 else
     echo
-    echo "Could not find a DB dump.sql file!"
+    echo "Could not find a DB file at /vagrant/dump.sql!"
     echo
 fi
