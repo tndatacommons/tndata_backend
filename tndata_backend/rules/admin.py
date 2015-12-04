634c14b38e5c1135102f70924b5b9792208b716e
Brad Montgomery <brad@bradmontgomery.net>
2015-12-04 15:09:18 -0600

    removing business rules based code

--- tndata_backend/rules/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-class RuleAdmin(admin.ModelAdmin):
-    list_display = ('app_name', 'rule_name', 'created', 'modified')
-admin.site.register(models.Rule, RuleAdmin)
