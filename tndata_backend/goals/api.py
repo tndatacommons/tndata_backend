cf87987da28065b80deb9b5694b05f1f42e5931e
Brad Montgomery <brad@bradmontgomery.net>
2016-01-05 17:03:23 -0600

    adding api docs

--- tndata_backend/goals/api.py
+++ tndata_backend/goals/api.py
@@ -1611,7 +1611,38 @@ class CustomGoalViewSet(mixins.CreateModelMixin,
                         mixins.UpdateModelMixin,
                         mixins.DestroyModelMixin,
                         viewsets.GenericViewSet):
-    """A User's custom goals.
+    """A User's custom goals.  This endpoint allows a user to retrieve a list
+    of or create new custom goals.
+
+    GET requests return an array of results containing the following:
+
+    * `id`: The `CustomGoal` ID.
+    * `user`: The ID for the goal's owner.
+    * `title`: The title of the custom goal.
+    * `title_slug`: A sluggified version of the title.
+    * `updated_on`: Date the goal was updated.
+    * `created_on`: Date the goal was created.
+    * `object_type`: This will always be the string, `customgoal`.
+
+    ## Creating Custom Goals.
+
+    Send a POST request to the [/api/users/customgoals/](/api/users/customgoals/)
+    endpoint with the following information:
+
+        {'title': 'Some Title for your custom goal'}
+
+    ## Custom Goal Details
+
+    You can retrieve details for a single custom goal instance via the
+    `/api/users/customgoals/{customgoal_id}/` endpoint, which will return a
+    single JSON representation for the object.
+
+    ## Updating Custom Goals.
+
+    Send a PUT request to the `/api/users/customgoals/{customgoal_id}/` endpoint,
+    containing the information that you wish to update, namely the title.
+
+        {'title': 'Updated Title'}
 
     ----
 
@@ -1643,7 +1674,56 @@ class CustomActionViewSet(mixins.CreateModelMixin,
                           mixins.UpdateModelMixin,
                           mixins.DestroyModelMixin,
                           viewsets.GenericViewSet):
-    """A User's custom actions.
+    """A User's custom actions. This endpoint allows a user to retrieve a list
+    of or create new custom actions. A `CustomAction` object must be associated
+    with a parent `CustomGoal` object (see [Custom Goals](/api/users/customgoals/)).
+
+    GET requests return an array of results containing the following:
+
+    * `id`: The ID of the `CustomAction` object.
+    * `user`: The ID of the action's owner
+    * `customgoal`: The ID of the parent `CustomGoal` object.
+    * `title`: Title of the custom action (required)
+    * `title_slug`: sluggified version of the title
+    * `notification_text`: Text that will be used in a notification (required)
+    * `custom_trigger`: A `Trigger` object
+    * `next_trigger_date`: The next date the notification will be sent.
+      May be `null`
+    * `prev_trigger_date`: The previous date the notficiation was sent.
+      May be `null`
+    * `updated_on`: Date the action was last updated.
+    * `created_on`: Date the action was created.
+    * `object_type`: Will always be the string, `customaction`.
+
+
+    ## Creating Custom Actions
+
+    Send a POST request to the
+    [/api/users/customactions/](/api/users/customactions/) endpoint with the
+    following data:
+
+        {
+            'title': 'Your action Title',
+            'notification_text': 'Your action's notification',
+            'customgoal': {ID of the parent custom goal},
+        }
+
+    ## Custom Action Details
+
+    You can retrieve details for a single custom action instance via the
+    `/api/users/customactions/{customaction_id}/` endpoint, which will return a
+    single JSON representation for the object.
+
+    ## Updating Custom Actions.
+
+    Send a PUT request to the `/api/users/customactions/{customaction_id}/`
+    endpoint, containing the information that you wish to, e.g.:
+
+        {
+            'title': 'Updated Title',
+            'notification_text': 'Updated notification',
+            'customgoal': {ID of the parent custom goal},
+        }
 
     ----
 
