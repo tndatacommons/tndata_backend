ecea1dfd6c685b990e25025c62febb05b9c0dbaf
Brad Montgomery <brad@bradmontgomery.net>
2016-01-19 13:52:02 -0600

    adding a method to read api docs from a separate file and dynamically update the ViewSet class's docstring

--- tndata_backend/utils/mixins.py
+++ tndata_backend/utils/mixins.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 
 
 class LoginRequiredMixin(object):
@@ -29,6 +30,24 @@ class VersionedViewSetMixin:
     If for some reason the class does not include the appropriate serializer
     class attribute, the default version will be returned.
 
+    ----
+
+    Versioned Docs (via docstrings)
+
+    This mixin also makes use of DRF's self-describing API feature, but provides
+    a method to include different docs for different versions of the api.
+
+    To enable this, a `get_docstring` method will look for a `docstring_prefix`
+    attribute which should be a directory containing your documentation. The
+    convention is that your doc is the viewset's name, lowercased, with the
+    version number tacked on the end; in a markdown file with a .md extension.
+
+    For example: `UserViewSet`'s documentation for version 2 should be in a
+    file named `userviewset_v2.md`. And if `docstring_prefix = "api_docs"`,
+    this mixin will load the file (from your project root):
+
+        api_docs/userviewset_v2.md
+
     """
     # Mapping of version number to local, expected attribute name
     _versions = {
@@ -45,9 +64,30 @@ class VersionedViewSetMixin:
             attr_name = "serializer_class_v{}".format(default)
             serializer_class = getattr(self, attr_name)
 
-        doc_file = "goals/api_docs/{}_v{}.md".format(
-            self.__class__.__name__.lower(),
-            self.request.version
-        )
-        self.__class__.__doc__ = open(doc_file).read()
+        # HACK: Dynamically write a new docstring based on the api version
+        # It really makes little sense to do this work here, but this is
+        # the appropriate part of this object's lifecycle to hook into this.
+        docstring = self.get_docstring()
+        if docstring:
+            self.__class__.__doc__ = docstring
         return serializer_class
+
+    def get_docstring(self):
+        """Dynamically load a markdown file based on the api version; its
+        content gets cached forever."""
+        docstring = None
+        docstring_prefix = getattr(self, 'docstring_prefix', None)
+        if docstring_prefix:
+            try:
+                doc_file = "{}/{}_v{}.md".format(
+                    docstring_prefix,
+                    self.__class__.__name__.lower(),
+                    self.request.version
+                )
+                docstring = cache.get(doc_file)
+                if docstring is None:
+                    docstring = open(doc_file).read()
+                    cache.set(doc_file, docstring, timeout=None)
+            except FileNotFoundError:
+                pass
+        return docstring
