c7125164b7bf545afdfda80d5be78038fcfc5925
Brad Montgomery <brad@bradmontgomery.net>
2016-01-28 16:23:44 -0600

    when we refresh UserAction objects, let's do the same with CustomActions

--- tndata_backend/goals/management/commands/refresh_useractions.py
+++ tndata_backend/goals/management/commands/refresh_useractions.py
@@ -1,13 +1,13 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from goals.models import UserAction
+from goals.models import CustomAction, UserAction
 
 logger = logging.getLogger("loggly_logs")
 
 
 class Command(BaseCommand):
-    help = 'Updates the UserAction.next_trigger_date field for stale entries.'
+    help = 'Updates the next_trigger_date field for stale UserActions and CustomActions.'
 
     def handle(self, *args, **options):
         count = 0
@@ -16,5 +16,14 @@ class Command(BaseCommand):
             ua.save(update_triggers=True)  # fields get refreshed on save.
 
         msg = "Refreshed Trigger Date for {0} UserActions".format(count)
+        logger.error(msg)
+        self.stderr.write(msg)
+
+        count = 0
+        for ca in CustomAction.objects.stale():
+            count += 1
+            ca.save()  # fields get refreshed on save.
+
+        msg = "Refreshed Trigger Date for {0} CustomActions".format(count)
         logger.error(msg)
         self.stderr.write(msg)
