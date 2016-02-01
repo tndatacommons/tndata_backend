ee3871585c3e8ac6ea7eaedba88386f88f0ee7f0
Brad Montgomery <brad@bradmontgomery.net>
2016-02-01 17:19:10 -0600

    rq queue functions

---
+++ tndata_backend/notifications/queue.py
@@ -0,0 +1,43 @@
+from datetime import timedelta
+from django.utils import timezone
+
+import django_rq
+
+
+def get_scheduler(queue='default'):
+    return django_rq.get_scheduler('default')
+
+scheduler = get_scheduler()
+
+
+def send(message_id):
+    """Given an ID for a GCMMessage object, send the message via GCM."""
+    from . models import GCMMessage
+    msg = GCMMessage.objects.get(pk=message_id)
+    msg.send()
+
+
+def enqueue(message, threshold=24):
+    """Given a GCMMessage object, add it to the queue of messages to be sent."""
+
+    job = None
+    now = timezone.now()
+    threshold = now + timedelta(hours=threshold)
+
+    # Only queue up messages for the next 24 hours
+    if now < message.deliver_on and message.deliver_on < threshold:
+        job = scheduler.enqueue_at(message.deliver_on, send, message.id)
+
+    # TODO: save the job ID on the GCMMessage, so if it gets re-enqueued we
+    # can cancel the original?
+
+    return job
+
+
+def messages():
+    """Return a list of jobs that are scheduled with their scheduled times.
+
+    Returned data is a list of (Job, datetime) tuples.
+
+    """
+    return scheduler.get_jobs(with_times=True)
