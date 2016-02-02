1c308a8d7adc3544725675089fa9ecd1b3d2a302
Brad Montgomery <brad@bradmontgomery.net>
2016-02-02 14:19:03 -0600

    typo fix

--- tndata_backend/notifications/serializers.py
+++ tndata_backend/notifications/serializers.py
@@ -10,7 +10,7 @@ class GCMDeviceSerializer(ObjectTypeModelSerializer):
         model = GCMDevice
         fields = (
             'id', 'user','device_name', 'device_id', 'registration_id',
-            'is_active', 'created_on', 'updated_on', 'oject_type',
+            'is_active', 'created_on', 'updated_on', 'object_type',
         )
         read_only_fields = ("id", "created_on", "updated_on")
 
