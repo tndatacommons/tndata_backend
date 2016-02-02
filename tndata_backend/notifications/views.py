5212efdd9fb852a9c7fc18c2342316d5a37f103b
Brad Montgomery <brad@bradmontgomery.net>
2016-02-02 17:01:27 -0600

    included additional message details in the dashboard

--- tndata_backend/notifications/views.py
+++ tndata_backend/notifications/views.py
@@ -1,10 +1,8 @@
-from datetime import timedelta
-
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
-from django.utils import timezone
 
 from . import queue
+from .models import GCMMessage
 
 
 @user_passes_test(lambda u: u.is_staff, login_url='/')
@@ -12,10 +10,26 @@ def dashboard(request):
     """A simple dashboard for enqueued GCM notifications."""
 
     jobs = queue.messages()  # Get the enqueued messages
+    ids = [job.args[0] for job, _ in jobs]
 
+    message_data = {}
+    fields = ['id', 'title', 'user__email', 'message']
+    messages = GCMMessage.objects.filter(pk__in=ids).values_list(*fields)
+    for msg in messages:
+        mid, title, email, message = msg
+        message_data[mid] = {
+            'id': mid,
+            'title': title,
+            'email': email,
+            'message': message,
+        }
+
+    jobs = (
+        (job, scheduled_for, message_data[job.args[0]])
+        for job, scheduled_for in jobs
+    )
     context = {
         'jobs': jobs,
         'metrics': ['GCM Message Sent', 'GCM Message Scheduled', ]
     }
     return render(request, "notifications/index.html", context)
-
