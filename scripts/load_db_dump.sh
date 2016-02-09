63a6c1838af369b65e03afd6068e019bdc120f3e
Brad Montgomery <brad@bradmontgomery.net>
2016-02-09 17:49:45 -0600

    adding a script for local dev (on my laptop) and revising the load db script to work with my laptop, too

--- scripts/load_db_dump.sh
+++ scripts/load_db_dump.sh
@@ -9,6 +9,11 @@ if [ -e "/vagrant/dump.sql" ]
     sudo -u postgres dropdb tndata_backend;
     sudo -u postgres createdb -E UTF8 -T template0 tndata_backend
     sudo -u postgres psql -h 127.0.0.1 -U tndata_backend -d tndata_backend -f /vagrant/dump.sql
+elif [ -e "/Users/brad/tndata/deployment_tools/dump.sql" ]
+    then
+    dropdb tndata_backend;
+    createdb -E UTF8 -T template0 tndata_backend
+    psql -h 127.0.0.1 -d tndata_backend -f /Users/brad/tndata/deployment_tools/dump.sql
 else
     echo
     echo "Could not find a DB file at /vagrant/dump.sql!"
