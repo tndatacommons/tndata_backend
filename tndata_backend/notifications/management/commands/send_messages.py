806fcc6aa66a17cbdb6f959df42f1f7cb2fbe410
Brad Montgomery <brad@bradmontgomery.net>
2016-02-11 09:02:35 -0600

    wrapping the old GCM send_messages command in a waffle switch so we can switch back to it if we need to

--- tndata_backend/notifications/management/commands/send_messages.py
+++ tndata_backend/notifications/management/commands/send_messages.py
@@ -1,4 +1,5 @@
 import logging
+import waffle
 
 from django.core.management.base import BaseCommand
 from notifications.models import GCMMessage
@@ -11,26 +12,27 @@ class Command(BaseCommand):
     help = 'Sends messages to GCM'
 
     def handle(self, *args, **options):
-        self.stderr.write("This command has been deprecated")
+        if waffle.switch_is_active('use-rqscheduler'):
+            self.stderr.write("This command has been deprecated")
+        else:
+            # Look for all the undelivered/non-errored messages.
+            messages = GCMMessage.objects.ready_for_delivery()
+            if messages.exists():
+                log_msg = "Sending {0} GCMMessages".format(messages.count())
+                logger.info(log_msg)
+                self.stdout.write("{0}\n".format(log_msg))
 
-        # Look for all the undelivered/non-errored messages.
-#        messages = GCMMessage.objects.ready_for_delivery()
-#        if messages.exists():
-#            log_msg = "Sending {0} GCMMessages".format(messages.count())
-#            logger.info(log_msg)
-#            self.stdout.write("{0}\n".format(log_msg))
-#
-#            for message in messages:
-#                try:
-#                    message.send()
-#                    log_message = "Sent to GCM: user: {0}, message: {1}".format(
-#                        message.user.id, message.message
-#                    )
-#                    logger.info(log_message)
-#                except Exception:
-#                    log_msg = "Failed to send GCMMEssage id = {0}".format(message.id)
-#                    logger.error(log_msg)
-#                    self.stdout.write("{0}\n".format(log_msg))
-#
-#            logger.info("Finished Sending GCM Notifications")
-#            self.stdout.write("Finished Sending GCM Notifications")
+                for message in messages:
+                    try:
+                        message.send()
+                        log_message = "Sent to GCM: user: {0}, message: {1}".format(
+                            message.user.id, message.message
+                        )
+                        logger.info(log_message)
+                    except Exception:
+                        log_msg = "Failed to send GCMMEssage id = {0}".format(message.id)
+                        logger.error(log_msg)
+                        self.stdout.write("{0}\n".format(log_msg))
+
+                logger.info("Finished Sending GCM Notifications")
+                self.stdout.write("Finished Sending GCM Notifications")
