62398ce1fde0402a9c4b77ff018e47716ba1fdd3
Brad Montgomery <brad@bradmontgomery.net>
2016-03-15 16:42:56 -0500

    allow restricting the refresh_useractions command by user

--- tndata_backend/goals/management/commands/refresh_useractions.py
+++ tndata_backend/goals/management/commands/refresh_useractions.py
@@ -1,6 +1,9 @@
 import logging
 
-from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+
 from goals.models import CustomAction, UserAction
 
 logger = logging.getLogger("loggly_logs")
@@ -9,9 +12,37 @@ logger = logging.getLogger("loggly_logs")
 class Command(BaseCommand):
     help = 'Updates the next_trigger_date field for stale UserActions and CustomActions.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--user',
+            action='store',
+            dest='user',
+            default=None,
+            help=("Restrict this command to the given User. "
+                  "Accepts a username, email, or id")
+        )
+
+    def _user_kwargs(self, user):
+        User = get_user_model()
+
+        kwargs = {}  # Keyword arguments that get passed to our action querysets.
+        if user:
+            try:
+                if user.isnumeric():
+                    criteria = Q(id=user)
+                else:
+                    criteria = (Q(username=user) | Q(email=user))
+                kwargs['user'] = User.objects.get(criteria)
+            except User.DoesNotExist:
+                msg = "Could not find user: {0}".format(user)
+                raise CommandError(msg)
+        return kwargs
+
     def handle(self, *args, **options):
         count = 0
-        for ua in UserAction.objects.stale():
+        kwargs = self._user_kwargs(options['user'])
+
+        for ua in UserAction.objects.stale(**kwargs):
             count += 1
             ua.save(update_triggers=True)  # fields get refreshed on save.
 
@@ -20,7 +51,7 @@ class Command(BaseCommand):
         self.stderr.write(msg)
 
         count = 0
-        for ca in CustomAction.objects.stale():
+        for ca in CustomAction.objects.stale(**kwargs):
             count += 1
             ca.save()  # fields get refreshed on save.
 
