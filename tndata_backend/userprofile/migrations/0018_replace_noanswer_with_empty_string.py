6221b482813d7a540ada846f0b507a3a2e112115
Brad Montgomery <brad@bradmontgomery.net>
2016-04-05 12:26:54 -0500

    starting a migration

---
+++ tndata_backend/userprofile/migrations/0018_replace_noanswer_with_empty_string.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userprofile', '0017_copy_profile_fields_from_survey'),
+    ]
+
+    operations = [
+    ]
