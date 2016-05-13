402a912f27cd35a11d56f7b1d34ee283ea345ddd
Brad Montgomery <brad@bradmontgomery.net>
2016-05-13 11:30:04 -0500

    add the ignore_types param to flatten so we can flatten lists of strings/bytes

--- tndata_backend/utils/datastructures.py
+++ tndata_backend/utils/datastructures.py
@@ -1,14 +1,14 @@
 from collections import Iterable
 
 
-def flatten(values):
+def flatten(values, ignore_types=(str, bytes)):
     """Flattens a nested list into a single list of values;
 
     Taken from Python Cookbook 3rd ed.
 
     """
     for item in values:
-        if isinstance(item, Iterable):
+        if isinstance(item, Iterable) and not isinstance(item, ignore_types):
             yield from flatten(item)
         else:
             yield item
