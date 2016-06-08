825b2f9fa91b6d9c2eb200b96bf3000faac5a5da
Brad Montgomery <brad@bradmontgomery.net>
2016-06-08 14:38:38 -0500

    integrating push notification when a badgify award is created

--- tndata_backend/notifications/signals.py
+++ tndata_backend/notifications/signals.py
@@ -1,4 +1,11 @@
 import django.dispatch
+import waffle
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+
+from badgify.models import Award
 
 # -----------------------------------------------------------------------------
 #
@@ -15,3 +22,27 @@ import django.dispatch
 
 snooze_args = ['message', 'user', 'related_object', 'deliver_on']
 notification_snoozed = django.dispatch.Signal(providing_args=snooze_args)
+
+# -----------------------------------------------------------------------------
+# Badgify-related signal handlers.
+# -----------------------------------------------------------------------------
+
+
+@receiver(post_save, sender=Award, dispatch_uid='badgify-award-ccreated')
+def badgify_award_created_send_notfication(sender, **kwargs):
+    """When a user is awarded a badge, we listen for Award.post_save, then
+    queue up a push notification for that award."""
+
+    created = kwargs.get('created', False)
+    award = kwargs.get('instance', False)
+
+    if created and award and waffle.switch_is_active('enable-badgify'):
+        from .models import GCMMessage
+        GCMMessage.objects.create(
+            user=award.user,
+            title=award.badge.name,
+            message=award.badge.description,
+            deliver_on=timezone.now(),
+            obj=award,
+            priority=GCMMessage.HIGH
+        )
