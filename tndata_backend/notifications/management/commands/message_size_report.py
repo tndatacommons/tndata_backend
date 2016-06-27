e8052e0ab693bf58eee6d6d6ee04efad3f1e9ff7
Brad Montgomery <brad@bradmontgomery.net>
2016-06-27 14:09:14 -0500

    updating the message size report

--- tndata_backend/notifications/management/commands/message_size_report.py
+++ tndata_backend/notifications/management/commands/message_size_report.py
@@ -1,59 +1,28 @@
 import array
-import json
 import statistics
-import sys
-
 from django.core.management.base import BaseCommand
-
 from notifications.models import GCMMessage
 
 
 class Command(BaseCommand):
-    help = 'Print some info about message sizes'
+    help = 'Print some info about push notification payload sizes'
 
     def handle(self, *args, **options):
         sizes = array.array('L')
-        expanded_sizes = array.array('L')
 
         for msg in GCMMessage.objects.all():
             # Store the size of the gcm payload for existing messages.
-            sizes.append(sys.getsizeof(msg.content_json))
-
-            # Then investigate including more info (e.g. embedding the action
-            # and behavior), and check on the resulting size.
-            if msg.content_object and msg.content_type and msg.content_type.name.lower() == "action":
-                payload = msg.content
-                action = msg.content_object
-                payload['action'] = {
-                    'id': action.id,
-                    'title': action.title,
-                    'description': action.description,
-                }
-                payload['behavior'] = {
-                    'id': action.behavior_id,
-                    'title': action.behavior.title,
-                    'description': action.behavior.description,
-                }
-                payload = json.dumps(payload)
-                expanded_sizes.append(sys.getsizeof(payload))
+            # The json-encoded string *should* be in the correct text encoding
+            sizes.append(len(msg.content_json))
 
         # Do some reporting.
         sizes = sorted(sizes)
         print("\n-------------------------------------------------------")
         print("For current notifications:")
-        print("- Mean: {} bytes".format(statistics.mean(sizes)))
-        print("- Median: {} bytes".format(statistics.median(sizes)))
-        print("- Mode: {} bytes".format(statistics.median(sizes)))
-        print("- Stdev: {}".format(statistics.stdev(sizes)))
-        print("- Smallest: {} bytes".format(min(sizes)))
-        print("- Largest: {} bytes".format(max(sizes)))
-
-        sizes = sorted(expanded_sizes)
-        print("\nIncluding Action/Behavior dataa within the message:")
-        print("- Mean: {} bytes".format(statistics.mean(sizes)))
-        print("- Median: {} bytes".format(statistics.median(sizes)))
-        print("- Mode: {} bytes".format(statistics.median(sizes)))
-        print("- Stdev: {}".format(statistics.stdev(sizes)))
+        print("- Mean: {} bytes".format(round(statistics.mean(sizes), 2)))
+        print("- Median: {} bytes".format(round(statistics.median(sizes), 2)))
+        print("- Mode: {} bytes".format(round(statistics.median(sizes), 2)))
+        print("- Stdev: {}".format(round(statistics.stdev(sizes), 2)))
         print("- Smallest: {} bytes".format(min(sizes)))
         print("- Largest: {} bytes".format(max(sizes)))
         print("-------------------------------------------------------\n")
