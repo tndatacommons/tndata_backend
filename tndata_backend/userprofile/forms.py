5cf94fc4fbde21b463c73e086247f2e245c36916
Brad Montgomery <brad@bradmontgomery.net>
2016-07-20 13:36:06 -0500

    add crispy forms to the UserForm and SetNewPasswordForm, fix template name typo

--- tndata_backend/userprofile/forms.py
+++ tndata_backend/userprofile/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Layout
+
 from .models import UserProfile
 
 
@@ -14,3 +17,28 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ('first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False  # Don't generate <form> tags
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    'email',
+                    css_class="large-12 small-12 columns"
+                ),
+                css_class="row"
+            ),
+            Div(
+                Div(
+                    'first_name',
+                    css_class="large-6 small-12 columns"
+                ),
+                Div(
+                    'last_name',
+                    css_class="large-6 small-12 columns"
+                ),
+                css_class="row"
+            )
+        )
--- tndata_backend/utils/forms.py
+++ tndata_backend/utils/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Layout
+
 
 class EmailForm(forms.Form):
     """A simple form that validates a single email address."""
@@ -17,6 +20,24 @@ class SetNewPasswordForm(forms.Form):
         widget=forms.PasswordInput,
         help_text="Confirm your new password"
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False  # Don't generate <form> tags
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    'password',
+                    css_class="large-6 small-12 columns"
+                ),
+                Div(
+                    'password_confirmation',
+                    css_class="large-6 small-12 columns"
+                ),
+                css_class="row"
+            )
+        )
 
     def clean(self):
         """Ensure passwords match."""
