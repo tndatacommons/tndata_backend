d4024088a8d19b9ca237029a73b06530b5cdee1e
Brad Montgomery <brad@bradmontgomery.net>
2016-07-29 17:02:30 -0500

    adding a util function to turn an interable into a DRF-formatted result set

--- tndata_backend/utils/serializers.py
+++ tndata_backend/utils/serializers.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from rest_framework import serializers
 
 
@@ -6,3 +7,29 @@ class ObjectTypeModelSerializer(serializers.ModelSerializer):
 
     def get_object_type(self, obj):
         return obj.__class__.__name__.lower()
+
+
+def resultset(iterable):
+    """Given any iterable, wrap it in the meta data expected to create a dict
+    result set, e.g.:
+
+    Turn this:
+
+        [{id: 1}, {id: 2}, ...]
+
+    Into:
+
+        {
+            count: 5,
+            next: null,
+            prev: null,
+            results: [
+                {id: 1}, {id: 2}, ...
+            ]
+        }
+
+    """
+    results = OrderedDict(count=0, next=None, prev=None, results=list())
+    results['count'] = len(iterable)
+    results['results'] = list(iterable)
+    return results
