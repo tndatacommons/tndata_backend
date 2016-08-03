7585976fced251ed6ce13198ca73c8c0ac5236db
Brad Montgomery <brad@bradmontgomery.net>
2016-08-03 12:03:55 -0500

    making cronlog entries read-only

--- tndata_backend/cronlog/admin.py
+++ tndata_backend/cronlog/admin.py
@@ -6,5 +6,6 @@ class CronLogAdmin(admin.ModelAdmin):
     list_display = ('command', 'message', 'created_on')
     list_filter = ('command', )
     search_fields = ['command', 'message']
+    readonly_fields = ('command', 'message', 'created_on')
 
 admin.site.register(CronLog, CronLogAdmin)
