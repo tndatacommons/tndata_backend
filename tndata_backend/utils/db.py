20c2b21bb202b02bae31ebf0f6aa2dec42a1298b
Brad Montgomery <brad@bradmontgomery.net>
2016-08-04 17:36:19 -0500

    adding a function to ciorrectly look up an object's model name

--- tndata_backend/utils/db.py
+++ tndata_backend/utils/db.py
@@ -17,3 +17,20 @@ def get_object_or_none(model, **kwargs):
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         return None
+
+
+def get_model_name(obj):
+    """Given an instance of a Model object, return the name (in all lower-case)
+    of the Model.
+
+    Example: `get_model_name(user) -> "user"`.
+
+    This correctly handles the case where an object may be a deferred object.
+
+    """
+    object_type = type(obj)
+    if object_type._deferred:
+        object_type = object_type.__base__.__name__
+    else:
+        object_type = object_type.__name__
+    return object_type.lower()
