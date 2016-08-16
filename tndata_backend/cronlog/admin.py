f4e1e459e6609620bf5cba9d546d406b62720627
Brad Montgomery <brad@bradmontgomery.net>
2016-08-16 12:10:30 -0500

    includ host on list display

--- tndata_backend/cronlog/admin.py
+++ tndata_backend/cronlog/admin.py
@@ -3,7 +3,7 @@ from . models import CronLog
 
 
 class CronLogAdmin(admin.ModelAdmin):
-    list_display = ('command', 'message', 'created_on')
+    list_display = ('command', 'message', 'host', 'created_on')
     list_filter = ('command', 'host', )
     search_fields = ['command', 'message', 'host']
     readonly_fields = ('command', 'message', 'host', 'created_on')
