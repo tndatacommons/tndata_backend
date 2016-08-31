f8099d476d396dd1a5bf1a8aaf2aca4e8784bfd8
Brad Montgomery <brad@bradmontgomery.net>
2016-08-31 15:17:22 -0500

    redirect to the program after editing it

--- tndata_backend/goals/views.py
+++ tndata_backend/goals/views.py
@@ -1351,7 +1351,7 @@ class ProgramUpdateView(StaffRequiredMixin, UpdateView):
 
     def get_success_url(self):
         messages.success(self.request, "Your program has been saved")
-        return self.object.organization.get_absolute_url()
+        return self.object.get_absolute_url()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
