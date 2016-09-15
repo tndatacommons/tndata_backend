dd79153498c66411e37b980ce9c1b97eb24ecb7a
Brad Montgomery <brad@bradmontgomery.net>
2016-09-15 16:47:07 -0500

    adding a few test for the rewards app, and fixing a bug that happens when there are no FunContent objects

--- tndata_backend/rewards/managers.py
+++ tndata_backend/rewards/managers.py
@@ -6,4 +6,7 @@ class FunContentManager(Manager):
     def random(self):
         """Return a random FunContent instance."""
         query = "SELECT * FROM rewards_funcontent ORDER BY random() LIMIT 1"
-        return self.raw(query)[0]
+        try:
+            return self.raw(query)[0]
+        except IndexError:
+            return None
--- tndata_backend/rewards/tests.py
+++ tndata_backend/rewards/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from .models import FunContent
 
-# Create your tests here.
+
+class TestFunContent(TestCase):
+
+    def setUp(self):
+        self.quote = FunContent.objects.create(
+            message_type='quote',
+            message="Here's a quote",
+            author="Brad",
+            keywords=['foo', 'bar'],
+        )
+
+    def test__str__(self):
+        expected = "Here's a quote"
+        actual = "{}".format(self.quote)
+        self.assertEqual(expected, actual)
+
+    def test__clean_keywords(self):
+        obj = FunContent(keywords=['   BAZ', 'binGO'])
+        obj._clean_keywords()
+        self.assertEqual(obj.keywords, ['baz', 'bingo'])
+
+
+class TestFunContentManager(TestCase):
+
+    def tearDown(self):
+        FunContent.objects.all().delete()
+
+    def test_with_exiting_content(self):
+        quote = FunContent.objects.create(
+            message_type='quote',
+            message="Here's a quote",
+            author="Brad",
+            keywords=['foo', 'bar'],
+        )
+
+        obj = FunContent.objects.random()
+        self.assertEqual(obj.id, quote.id)
+
+    def test_with_no_content(self):
+        """If tehre are no FunContent items this should return None"""
+        obj = FunContent.objects.random()
+        self.assertIsNone(obj)
