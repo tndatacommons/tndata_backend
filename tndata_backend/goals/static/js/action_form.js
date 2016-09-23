8ea53207093b0ff483b9ee81d7bba86d06606b72
Brad Montgomery <brad@bradmontgomery.net>
2016-09-23 16:33:14 -0500

    revising action templates and js for form

--- tndata_backend/goals/static/js/action_form.js
+++ tndata_backend/goals/static/js/action_form.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     // If we've loaded the page, and there's a title but not notification.
     if(notification_field.val() === "" && original_title !== "") {
         notification_field.val(original_title);
-        console.log("Setting notification to: " + original_title);
     }
 
     // See if we should change the notification.
@@ -20,39 +19,12 @@ $(document).ready(function() {
             // if we're setting the title field for the first time,
             // copy it's value into the notification field.
             notification_field.val(title_field.val());
-            console.log("Setting notification to: " + title_field.val());
         } else if(notification_field.val() === original_title) {
             // if we're changing the title AND it originally matched the
             // notification field.
             notification_field.val(title_field.val());
-            console.log("Setting notification to: " + title_field.val());
         }
     });
-
-
-    // Function to toggle the selected Behavior's details.
-    var toggle_behavior_details = function(selector) {
-        $("#behavior-info").show();
-        $("#behaviors-table").show();
-        $(".behaviors").hide(); // hide all other behaviors.
-        $(selector).show(); // show the selected one.
-        $("#behavior-info h2").text($("#id_behavior option:selected").text());
-        $('.existing-action').hide();
-        $('.existing-action.'+selector.replace("#", "")).show();
-    };
-
-    // We we select a Behavior, display some of it's info.
-    $("#id_behavior").change(function() {
-        var selected_id = "#behavior-" + $(this).val();
-        toggle_behavior_details(selected_id);
-    });
-
-    // Show any pre-selected behaviors.
-    var selected_id = $("#id_behavior").val();
-    if(selected_id) {
-        selected_id = "#behavior-" + selected_id;
-        toggle_behavior_details(selected_id);
-    }
 
     // Display the resource fieldset when we have a Resource action
     $("#id_action_type").change(function() {
