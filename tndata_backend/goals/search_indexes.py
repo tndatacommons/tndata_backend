c813bb1f7afbf89741e404301161321406c2b27d
Brad Montgomery <brad@bradmontgomery.net>
2016-09-23 17:32:38 -0500

    removing behavior from search_index

--- tndata_backend/goals/search_indexes.py
+++ tndata_backend/goals/search_indexes.py
@@ -1,6 +1,6 @@
 import json
 from haystack import indexes
-from goals.models import Action, Behavior, Category, Goal
+from goals.models import Action, Category, Goal
 
 # -----------------------------------------------------------------------------
 # NOTE on haystack fields; see https://goo.gl/1S79dX
@@ -65,29 +65,6 @@ class CategoryIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.published(packaged_content=False)
-
-
-class BehaviorIndex(indexes.SearchIndex, indexes.Indexable):
-    """A search index for all published Behavior."""
-    text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    description = indexes.CharField(model_attr='description')
-    url = indexes.CharField(model_attr='get_absolute_url')
-    updated_on = indexes.DateTimeField(model_attr='updated_on')
-    serialized_object = indexes.CharField()  # Serialized object details.
-
-    def prepare_serialized_object(self, obj):
-        # This sucks; serializers import models, as do indexes, but serializers
-        # also import the index, so we've got a circular dependency.
-        from goals.serializers import v2
-        return json.dumps(v2.BehaviorSerializer(obj).data)
-
-    def get_model(self):
-        return Behavior
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.published()
 
 
 class ActionIndex(indexes.SearchIndex, indexes.Indexable):
