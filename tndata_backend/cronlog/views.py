5dca0913cabb66461c1221f964bcb408f94f4338
Brad Montgomery <brad@bradmontgomery.net>
2016-09-27 16:48:12 -0500

    if django doesn't handle the posted payload, assume it's json and deserialize it from the request body

--- tndata_backend/cronlog/views.py
+++ tndata_backend/cronlog/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -13,30 +15,33 @@ KEY = "las98a890se4na908sdfanm32"
 @csrf_exempt
 def add_entry(request):
     results = {}
-    if (
-        request.method == "POST" and
-        request.POST.get('key') == KEY and
-        request.POST.get('command') and
-        request.POST.get('message')
-    ):
-        host = request.POST.get('host', request.get_host())
-        item = CronLog.objects.create(
-            command=request.POST['command'],
-            message=request.POST['message'],
-            host=host
-        )
-        return JsonResponse({
-            'command': item.command,
-            'message': item.message,
-            'host': item.host,
-            'created_on': item.created_on,
-        })
-    elif request.method == "POST" and request.POST.get('key') is None:
-        return JsonResponse({'error': 'Missing Key'}, status=400)
-    elif (
-        request.method == "POST" and
-        ('command' not in request.POST or 'message' not in request.POST)
-    ):
-        results = {'error': 'command and message are required'}
-        return JsonResponse(results, status=400)
+    if request.method == "POST":
+        # Try grabbing the POST data.
+        try:
+            data = dict(request.POST)
+            if data == {}:  # No data? Try de-coding the post body
+                data = json.loads(request.body.decode("utf8"))
+
+            key = data['key']
+            command = data['command']
+            message = data['message']
+            host = data.get('host', request.get_host())
+
+            # Check for our key
+            if key == KEY and command and message:
+                item = CronLog.objects.create(command=command, message=message, host=host)
+                return JsonResponse({
+                    'command': item.command,
+                    'message': item.message,
+                    'host': item.host,
+                    'created_on': item.created_on,
+                })
+            elif key == KEY:
+                results = {'error': 'command and message are required'}
+                return JsonResponse(results, status=400)
+            else:
+                return JsonResponse({'error': 'Missing Key'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'error': 'Invalid Payload'}, status=400)
     return JsonResponse(results)
