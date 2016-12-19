b13d40f497705f99656d6d3eb785a851e8640443
Brad Montgomery <brad@bradmontgomery.net>
2016-12-19 16:14:37 -0600

    adding a chatmessage model

--- tndata_backend/chat/admin.py
+++ tndata_backend/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ChatMessage
 
-# Register your models here.
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'room', 'read', 'created_on')
+    raw_id_fields = ('from_user', 'to_user')
--- tndata_backend/chat/models.py
+++ tndata_backend/chat/models.py
@@ -1,3 +1,25 @@
+import logging
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+logger = logging.getLogger(__name__)
+
+
+class ChatMessage(models.Model):
+    """A persisted chat message."""
+
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sent_chat_messages")
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="received_chat_messages")
+    room = models.CharField(max_length=256, default="")
+    message = models.TextField(default="")
+    read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = "Chat Message"
+        verbose_name_plural = "Chat Messages"
