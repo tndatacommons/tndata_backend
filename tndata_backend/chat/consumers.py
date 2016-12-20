5400dbbeeda0d51137a0a51db147f75887042753
Brad Montgomery <brad@bradmontgomery.net>
2016-12-20 12:43:56 -0600

    revise consumers so we've got messages flying around but a ChatMEsasge object getting created asyncrhonously

--- tndata_backend/chat/consumers.py
+++ tndata_backend/chat/consumers.py
@@ -1,47 +1,119 @@
-from channels import Group
+from channels import Channel, Group
 from channels.handler import AsgiHandler
-from channels.sessions import channel_session
+from channels.sessions import channel_session, enforce_ordering
+from channels.auth import http_session_user, channel_session_user, channel_session_user_from_http
 
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.utils.html import escape
 
+from .models import ChatMessage
+
 from clog.clog import clog
 
+# TODO: figure out how to create rooms for 1-1 chat between 2 users. (do we need a Group?)
+# TODO: figure out how to create a ChatMessage object for chats.
+# TODO: ^^ how to retrive the user data so we can store that on the ChatMessage
+# TODO: figure out how to auto-display the channel's last 10 messages or so.
 
-# TODO: -----------------------------------------------------------------------
-# TODO: Pick up with Authentication in the docs:
-# TODO: https://channels.readthedocs.io/en/stable/getting-started.html#authentication
-# TODO: -----------------------------------------------------------------------
+def chat_message_consumer(message):
+    """Given a message, this creates a DB object and sends the message to a group.
+
+    The given message should have the following content:
+
+        - user_id: ID of the user who created the message.
+        - room: name of the room the message was sent to.
+        - text: text of the message.
+
+    """
+
+    clog({
+        "message.content": message.content,
+    }, title="Message in msg_consumer", color="green")
+
+    # Save the model
+    try:
+        User = get_user_model()
+        user = User.objects.get(pk=message.content['user_id'])
+        ChatMessage.objects.create(
+            user=user,
+            room=message.content['room'],
+            text=message.content['text'],
+        )
+    except (User.DoesNotExist, KeyError) as e:
+        clog(e, color='red')
 
 
-# Connected to websocket.connect
-@channel_session
-def ws_connect(message):
-    # Work out room name from path (ignore slashes)
-    room = message.content['path'].strip("/")
-
-    clog(message, title="CONNECT", color="green")
-    clog(room, title="Room", color="magenta")
-
-    # Save room in session and add us to the group
-    message.channel_session['room'] = room
-    Group("chat-%s" % room).add(message.reply_channel)
-
-
-# Connected to websocket.receive
-@channel_session
+@enforce_ordering(slight=True)
+@channel_session_user  # Gives us a channel_session + user
 def ws_message(message):
+    """Handles received messages to a websocket."""
+    # Stick the message on the processing queue (instead of sending
+    # it directly to the group)
     room = message.channel_session['room']
 
-    clog(message.content, title="[{}] message content".format(room))
-    Group("chat-%s" % room).send({
-        "text": "{}".format(escape(message['text'])),  # message.content['text']?
+    # Log some stuff so we know what's happening.
+    content_data = {
+        'room': room,
+        'from': message.user,
+        'message.content': message.content,
+    }
+    clog(content_data, title="Message Content")
+
+    payload = {
+        "text": "{}: {}".format(message.user.first_name, escape(message['text']))
+    }
+    Group(room).send(payload)  # send to users for display
+
+    # Send it to the channel to create the ChatMessage.
+    Channel("create-chat-message").send({
+        "room": room,
+        "text": message['text'],
+        "user_id": message.user.id,
     })
 
 
-# Connected to websocket.disconnect
-@channel_session
+@enforce_ordering(slight=True)
+@channel_session_user_from_http  # Give us session + user from http session.
+def ws_connect(message):
+    """Handles when clients connect to a websocket.
+    Connected to the `websocket.connect` channel."""
+
+    # XXX: Work out room name from path (ignore slashes)
+    room = "chatroom-{}".format(message.content['path'].strip('/'))
+
+    # XXX: Interesting attributes on message.content
+    # message.content['order']
+    # message.content['path']  # e.g. /chat
+    # message.content['query_string']
+    # message.content['reply_channel']
+    connect_data = {
+        'room': room,
+        'user_id': message.user.id,
+        'path': message.content['path'],
+        'query_string': message.content['query_string'],
+        'reply_channel': message.content['reply_channel'],
+    }
+    clog(connect_data, title="On Connection", color="magenta")
+
+    # Save room in session and add us to the group
+    message.channel_session['room'] = room
+    Group(room).add(message.reply_channel)
+
+
+@enforce_ordering(slight=True)
+@channel_session_user  # Gives us a session store + a user
 def ws_disconnect(message):
-    clog(message, title="DISCONNECT", color="red")
-    #Group("chat").discard(message.reply_channel)
-    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
+    """Handles when clients disconnect from a websocket.
+    Connected to the `websocket.disconnect` channel."""
+
+    # Pull the room from the channel's session.
+    room = message.channel_session['room']
+
+    disconnect_data = {
+        'room': room,
+        'user_id': message.user.id,
+        'message.content': message.content
+    }
+    clog(disconnect_data, title="DISCONNECTED", color="red")
+    Group(room).discard(message.reply_channel)
--- tndata_backend/config/routing.py
+++ tndata_backend/config/routing.py
@@ -3,13 +3,15 @@ This module defines routing for django `channels`.
 """
 from channels.routing import route
 from chat.consumers import ws_connect, ws_disconnect, ws_message
+from chat.consumers import chat_message_consumer
 
 
 channel_routing = [
     # This takes over all http requests.
-    # route("http.request", "chat.consumers.http_consumer"),
+    #route("http.request", "chat.consumers.http_consumer"),
 
     route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
     route("websocket.disconnect", ws_disconnect),
+    route("create-chat-message", chat_message_consumer),
 ]
