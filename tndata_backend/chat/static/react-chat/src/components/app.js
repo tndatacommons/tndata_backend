2c7070467c2b8bdb1f89f4532390fdef3ed04232
Brad Montgomery <brad@bradmontgomery.net>
2016-12-21 11:59:17 -0600

    cleaning up layout and ensure we're  connecting on the right port

--- tndata_backend/chat/static/react-chat/src/components/app.js
+++ tndata_backend/chat/static/react-chat/src/components/app.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 import Chat from './chat';
 
 
+// Ensure our chat app hits the api running on the same host
 // e.g. ==> ws://127.0.0.1:8000
-const WS_HOST = 'ws://' + window.location.hostname + ':' + window.location.port
+//
+// TODO: now to know if we're using port 8000, or 80, or 443 or what?
+let PORT = window.location.port;
+if(PORT.length > 0) {
+    PORT = ":8000";
+}
+const WS_HOST = 'ws://' + window.location.hostname + PORT;
 
 
 class App extends Component {
@@ -13,13 +20,8 @@ class App extends Component {
     console.log("[WEBSOCKET] " + ws_url);
 
     return (
-      <div className="App">
-        <div className="App-header">
-            <h1>Chat</h1>
-        </div>
-        <div className="App-content">
-            <Chat ws_url={ws_url}/>
-        </div>
+      <div>
+        <Chat ws_url={ws_url}/>
       </div>
     );
   }
