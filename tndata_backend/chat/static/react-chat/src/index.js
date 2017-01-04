8730b6dd7a9b2981d2300aa0024fc02f38d608d9
Brad Montgomery <brad@bradmontgomery.net>
2017-01-04 17:15:58 -0600

    killing some whitespace

--- tndata_backend/chat/static/react-chat/src/index.js
+++ tndata_backend/chat/static/react-chat/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/app';
-
-
 
 const rootEl = document.getElementById("root");
 const apiToken = rootEl.getAttribute("data-token");
