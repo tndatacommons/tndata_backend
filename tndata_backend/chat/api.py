dd422752c57e9fabc76aee74ced80144aea25743
Brad Montgomery <brad@bradmontgomery.net>
2017-01-05 15:20:00 -0600

    endpoint to list unread messages for a user

--- tndata_backend/chat/api.py
+++ tndata_backend/chat/api.py
@@ -1,11 +1,13 @@
 import logging
 
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, status, viewsets
 from rest_framework.authentication import (
     SessionAuthentication,
     TokenAuthentication
 )
+from rest_framework.decorators import detail_route, list_route
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from .models import ChatMessage
 from .serializers import ChatMessageSerializer
@@ -36,3 +38,19 @@ class ChatMessageViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         self.queryset = super().get_queryset().filter(user=self.request.user)
         return self.queryset
+
+    @list_route(methods=['get'], url_path='unread')
+    def unread(self, request, pk=None):
+        """List the current user's unread chat messages."""
+        if not request.user.is_authenticated():
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        messages = ChatMessage.objects.filter(
+            read=False,
+            room__icontains=request.user.username,
+        )
+        content = {
+            'count': messages.count(),
+            'results': ChatMessageSerializer(messages, many=True).data,
+        }
+        return Response(content, status=status.HTTP_200_OK)
