bbe5918fb056d7b9851e5ca48e80ce4581209fa2
Brad Montgomery <brad@bradmontgomery.net>
2017-01-05 15:36:42 -0600

    including chat message author's username/full name

--- tndata_backend/chat/serializers.py
+++ tndata_backend/chat/serializers.py
@@ -6,3 +6,10 @@ class ChatMessageSerializer(ObjectTypeModelSerializer):
     class Meta:
         model = ChatMessage
         fields = ('id', 'user', 'room', 'text', 'read', 'created_on')
+
+    def to_representation(self, obj):
+        """Include the author's username + full name in the message"""
+        results = super().to_representation(obj)
+        results['user_username'] = obj.user.username
+        results['user_full_name'] = obj.user.get_full_name()
+        return results
