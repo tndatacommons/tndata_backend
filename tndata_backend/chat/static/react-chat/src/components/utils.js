c90c3578f721c892c59bca64322e9b69832ece4c
Brad Montgomery <brad@bradmontgomery.net>
2017-01-06 15:28:07 -0600

    extracting utils from the chat component

---
+++ tndata_backend/chat/static/react-chat/src/components/utils.js
@@ -0,0 +1,70 @@
+import _ from 'lodash';
+
+/*
+ * Stolen from:
+ * https://gist.github.com/mathewbyrne/1280286
+ *
+ *
+ * TODO: Read thru this, instead, & see if we should use something like it.
+ * https://github.com/raineroviir/react-redux-socketio-chat/blob/master/src/common/containers/ChatContainer.js
+ *
+ *
+ */
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
+/*
+ * If the given text contains a link to a youtube video, this function
+ * will parse the text and extract two bits of data:
+ *
+ * 1. The video URL, and
+ * 2. The video ID.
+ *
+ * If matched, this function returns an object of the form:
+ *
+ *  {
+ *      videoId: '...',
+ *      embedUrl: '...',
+ *  }
+ *
+ * Otherwise, it returns a false value.
+ *
+ * This current works for both short, and long share urls:
+ * - https://www.youtube.com/watch?v=fd6clpIvrfg
+ * - https://youtu.be/fd6clpIvrfg
+ */
+function extractVideo(text) {
+    let result = false;
+    let videoId = null;
+    let videoLink = null;
+    const short_re = /https:\/\/youtu.be\/(\w+)/;
+    const long_re = /https:\/\/www.youtube.com\/watch\?v=(\w+)/;
+
+    if(short_re.test(text)) {
+        // Note: returns ["https://youtu.be/fd6clpIvrfg", "fd6clpIvrfg"]
+        [videoLink, videoId] = short_re.exec(text);
+        result = {
+            videoId: videoId,
+            videoLink: videoLink,
+            embedUrl: "https://www.youtube.com/embed/" + videoId
+        }
+    }
+    else if(long_re.test(text)) {
+        [videoLink, videoId] = long_re.exec(text);
+        result = {
+            videoId: videoId,
+            videoLink: videoLink,
+            embedUrl: "https://www.youtube.com/embed/" + videoId
+        }
+    }
+    return result;
+}
+
+export { slugify, extractVideo };
