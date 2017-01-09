4a5370546792da7cddbe7dfe5a9b6394788175fe
Brad Montgomery <brad@bradmontgomery.net>
2017-01-09 17:10:09 -0600

    adding command to change ChatMessage room names to new format

---
+++ tndata_backend/chat/management/commands/convert_chat_message_rooms.py
@@ -0,0 +1,31 @@
+import logging
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+from chat.models import generate_room_name, ChatMessage
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = (
+        "Converts ChatMessage room names so they're based on User.id instead "
+        "of User.username"
+    )
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        for msg in ChatMessage.objects.all():
+            username_a, username_b = msg.room.split('-')[1:]
+            try:
+                user_a = User.objects.get(username=username_a)
+                user_b = User.objects.get(username=username_b)
+
+                msg.room = generate_room_name((user_a, user_b))
+                msg.save()
+            except (User.DoesNotExist, ValueError):
+                err = "Could not find users for room: {}".format(msg.room)
+                self.stdout.write(err)
+                logger.info(err)
