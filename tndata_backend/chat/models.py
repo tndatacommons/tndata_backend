43385ed014d345a182463a295811a00e22f53ee0
Brad Montgomery <brad@bradmontgomery.net>
2017-01-09 20:28:52 -0600

    ensure all parts of room names are strings

--- tndata_backend/chat/models.py
+++ tndata_backend/chat/models.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def generate_room_name(values):
     """Given an iterable of values that can either be strings or User objects,
     generate a room name."""
-    values = [v.id if hasattr(v, 'id') else v for v in values]
+    values = [str(v.id) if hasattr(v, 'id') else str(v) for v in values]
     values = sorted(values)
     return slugify("chat-{}-{}".format(*values))
 
