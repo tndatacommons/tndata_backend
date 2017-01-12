28e4e67489d2529fac3a9a16eeb9f6a60ff2af77
Brad Montgomery <brad@bradmontgomery.net>
2017-01-12 17:23:09 -0600

    slight refactor

--- tndata_backend/cronlog/views.py
+++ tndata_backend/cronlog/views.py
@@ -7,6 +7,36 @@ from django.views.decorators.csrf import csrf_exempt
 from . models import CronLog
 
 
+def get_payload(request):
+    """Given a request object, extract the data we want. Return a tuple
+    of the form:
+
+        (key, command, message, host)
+
+    """
+    _default_host = request.get_host()
+
+    # Try grabbing directlyf rom the POST data.
+    key = request.POST.get('key')
+    command = request.POST.get('command')
+    message = request.POST.get('message')
+    host = request.POST.get('host', _default_host)
+
+    # No data? Try de-coding the post body
+    if not any([key, command, message]) and dict(request.POST) == {}:
+        try:
+            data = json.loads(request.body.decode("utf8"))
+            key = data['key']
+            command = data['command']
+            message = data['message']
+            host = data.get('host', _default_host)
+        except KeyError:
+            command = None
+            message = None
+
+    return (key, command, message, host)
+
+
 # -----------------------------------------------------------------------------
 # This pseudo-api doesn't use CSRF protection, because we want to insert
 # data from a curl command so that request MUST include the folowing key.
@@ -19,32 +49,25 @@ from . models import CronLog
 def add_entry(request):
     results = {}
     if request.method == "POST":
-        # Try grabbing the POST data.
-        try:
-            data = dict(request.POST)
-            if data == {}:  # No data? Try de-coding the post body
-                data = json.loads(request.body.decode("utf8"))
+        key, command, message, host = get_payload(request)
 
-            key = data['key']
-            command = data['command']
-            message = data['message']
-            host = data.get('host', request.get_host())
-
-            # Check for our key
-            if key == settings.CRONLOG_KEY and command and message:
-                item = CronLog.objects.create(command=command, message=message, host=host)
-                return JsonResponse({
-                    'command': item.command,
-                    'message': item.message,
-                    'host': item.host,
-                    'created_on': item.created_on,
-                })
-            elif key == settings.CRONLOG_KEY:
-                results = {'error': 'command and message are required'}
-                return JsonResponse(results, status=400)
-            else:
-                return JsonResponse({'error': 'Missing Key'}, status=400)
-
-        except KeyError:
+        # Check for our key
+        if key == settings.CRONLOG_KEY and command and message:
+            params = {
+                'command': command,
+                'message': message,
+                'host': host,
+            }
+            item = CronLog.objects.create(**params)
+            return JsonResponse({
+                'command': item.command,
+                'message': item.message,
+                'host': item.host,
+                'created_on': item.created_on,
+            })
+        elif key == settings.CRONLOG_KEY:
+            results = {'error': 'command and message are required'}
+            return JsonResponse(results, status=400)
+        else:
             return JsonResponse({'error': 'Invalid Payload'}, status=400)
     return JsonResponse(results)
