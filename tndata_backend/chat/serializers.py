e7870bc642c9a42fe878b47a4ac8815f4cc40363
Brad Montgomery <brad@bradmontgomery.net>
2017-01-20 11:43:43 -0600

    include digest in chat api

--- tndata_backend/chat/serializers.py
+++ tndata_backend/chat/serializers.py
@@ -5,7 +5,7 @@ from . models import ChatMessage
 class ChatMessageSerializer(ObjectTypeModelSerializer):
     class Meta:
         model = ChatMessage
-        fields = ('id', 'user', 'room', 'text', 'read', 'created_on')
+        fields = ('id', 'user', 'room', 'text', 'read', 'digest', 'created_on')
 
     def to_representation(self, obj):
         """Include the author's username + full name in the message"""
