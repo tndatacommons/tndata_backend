74111fbb02cca86f6016dbf95d90db52995d35e7
Brad Montgomery <brad@bradmontgomery.net>
2017-01-26 08:50:36 -0600

    refactoring util functions out of consumers.py into utils.py

--- tndata_backend/chat/consumers.py
+++ tndata_backend/chat/consumers.py
@@ -1,8 +1,6 @@
 """
 These consumers handle our chat communications over a websocket.
 
-
-
 """
 import hashlib
 import json
@@ -11,139 +9,28 @@ from channels import Channel, Group
 from channels.sessions import enforce_ordering
 from channels.auth import channel_session_user, channel_session_user_from_http
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from django_rq import get_connection
 from redis_metrics import metric
-from pprint import pformat
 
 from .models import ChatMessage
-from .utils import generate_room_name
-
-
-def _log_messages(payload, key='websocket-debug'):
-    """Log all websocket traffic for debugging purposes."""
-    if settings.DEBUG or settings.STAGING:
-        conn = get_connection('default')
-        info = "{}|{}".format(timezone.now().strftime("%c"), pformat(payload))
-        conn.lpush(key, info)
-
-
-def _get_user(message):
-    """This function inspects the content of a message and attempts to return
-    the User object who authored the message. There are serveral strategies
-    for doing this:
-
-    1. If `message.user` is an autenticated users, return it.
-    2. See if the User's ID is stored in a channel session and look them up.
-    3. Attempt to lookup an auth token stored in the message's query string
-       or in the headers (available on websocket.connect)
-    4. See if the message's content contains a JSON-encoded string with a
-       token value.
-
-    If any of the above succeed, this function returns a User object. Otherwise,
-    it returns `message.user` which is probably an AnonymousUser object.
-
-    """
-    # 1. Return the authenticated user.
-    if message.user and message.user.is_authenticated():
-        return message.user
-
-    User = get_user_model()
-
-    # 2. Retrieve from channel session.
-    if 'user_id' in message.channel_session:
-        try:
-            return User.objects.get(pk=message.channel_session['user_id'])
-        except User.DoesNotExist:
-            pass
-
-    # 3a. Look for a token in a header
-    token = None
-    try:
-        headers = [
-            (k.decode('utf-8'), v.decode('utf-8'))
-            for k, v in message.content['headers']
-        ]
-        headers = [v for k, v in headers if k.lower() == 'authorization']
-        token = headers[0].split()[1]  # 'Token ACTUAL_TOKEN_STRING'
-    except (KeyError, IndexError, AttributeError):
-        pass
-
-    # 3b. Look for a token in a query string
-    try:
-        # NOTE: this is the only query string object supported.
-        token = message.content.get('query_string').split("token=")[1]
-    except (KeyError, IndexError, AttributeError):
-        pass
-
-    try:
-        if token:
-            return User.objects.get(auth_token__key=token)
-    except User.DoesNotExist:
-        pass
-
-    # 4. See if there's a token or ID in the message content.
-    try:
-        payload = json.loads(message.content.get('text', None))  # Poss. TypeError
-        if 'token' in payload:
-            return User.objects.get(auth_token__key=payload['token'])
-        elif 'from' in payload and payload['from'].isnumeric():
-            return User.objects.get(pk=payload['from'])
-    except (TypeError, IndexError, ValueError, User.DoesNotExist):
-        pass
-
-    # Return None if we didn't find a user. Don't allow Anonymous Users.
-    return None
-
-
-def _get_user_details(user):
-    """Return a tuple of (name, avatar)"""
-    try:
-        name = user.get_full_name()
-        avatar = (
-            user.userprofile.google_image.replace('https:', '').replace('http:', '')
-            or '//www.gravatar.com/avatar/0?d=mm&s=30'
-        )
-    except AttributeError:
-        name = "Anonymous"
-        avatar = '//www.gravatar.com/avatar/0?d=mm&s=30'
-    return (name, avatar)
-
-
-def _decode_text(message):
-    """Given a message, attempt to decode the JSON string and return the TEXT
-    of the message (what the user should see).
-
-    Returns a tuple of the type:
-
-        (message_content, message_type)
-
-    where message type is one of the following:
-
-        - "message": This is a ChatMessage the user should see.
-        - "receipt": This is a read-receipt message (not visible to the user)
-
-    """
-    try:
-        received = json.loads(message.content['text'])
-        if 'received' in received:
-            return (received['received'], 'receipt')
-        return (received['text'], 'message')
-    except (IndexError, ValueError):  # wasnt' JSON-encoded?
-        return message.content.get('text', '')
+from .utils import (
+    decode_message_text,
+    generate_room_name,
+    get_user_details,
+    get_user_from_message,
+    log_messages_to_redis,
+)
 
 
 def chat_message_consumer(message):
-    """Given a message, this creates a DB object and sends the message to a group.
+    """Given a message, this creates a DB object and sends the message to a
+    group. The given message should have the following content:
 
-    The given message should have the following content:
-
-        - user_id: ID of the user who created the message.
-        - room: name of the room the message was sent to.
-        - text: text of the message.
+    - user_id: ID of the user who created the message.
+    - room: name of the room the message was sent to.
+    - text: text of the message.
 
     """
     try:
@@ -161,7 +48,7 @@ def mark_as_read_consumer(message):
     """Given a message, query the DB for the matching ChatMessage and mark
     it as read. The given message should have the following content:
 
-        - digest: text of the message.
+    - digest: text of the message.
 
     """
     try:
@@ -174,30 +61,31 @@ def mark_as_read_consumer(message):
 @enforce_ordering(slight=True)
 @channel_session_user  # Gives us a channel_session + user
 def ws_message(message):
-    """Handles received messages to a websocket."""
-    _log_messages(message.content)
+    """Handle messages received by the websocket. This consumer figures out
+    what to do with chat messages that are sent by clients (either JS or mobile)
 
-    # `message` attributes and info.
-    # ------------------------------
-    # message.channel - the channel object.
-    # message.channel_layer -
-    # message.channel_session - Sessions, but for channels.
-    # message.content - dict of the stuff we're usually interested in:
-    #   {
-    #       'order': 1,
-    #       'path': '/chat/995/',
-    #       'reply_channel': 'websocket.send!fMCqdsWviiwR',
-    #       'text': JSON-encoded string.
-    #   }
+    For reference, the following are important `message` attributes and info.
 
-    # Stick the message on the processing queue (instead of sending
-    # it directly to the group)
+    - message.channel - the channel object.
+    - message.channel_layer -
+    - message.channel_session - Sessions, but for channels.
+    - message.content - dict of the stuff we're usually interested in:
+
+        {
+            'order': 1,
+            'path': '/chat/995/',
+            'reply_channel': 'websocket.send!fMCqdsWviiwR',
+            'text': JSON-encoded string.
+        }
+
+    """
+    log_messages_to_redis(message.content)
+
     room = message.channel_session.get('room')
     if room:
-
         # Look up the user that sent the message
-        user = _get_user(message)
-        name, avatar = _get_user_details(user)
+        user = get_user_from_message(message)
+        name, avatar = get_user_details(user)
 
         # ---------------------------------------------------------------------
         # The following is the current format for our recieved message data.
@@ -215,7 +103,7 @@ def ws_message(message):
         #       received: digest
         #   }
         # ---------------------------------------------------------------------
-        message_text, message_type = _decode_text(message)
+        message_text, message_type = decode_message_text(message)
 
         if message_type == 'message':
             # Construct message sent back to the client.
@@ -231,7 +119,7 @@ def ws_message(message):
                 'digest': digest,
             }
 
-            # send to users for display
+            # Send to users for display
             Group(room).send({'text': json.dumps(payload)})
 
             # Now, send it to the channel to create the ChatMessage object.
@@ -253,14 +141,23 @@ def ws_message(message):
 def ws_connect(message):
     """Handles when clients connect to a websocket.
     Connected to the `websocket.connect` channel."""
-    _log_messages(message.content)
+    log_messages_to_redis(message.content)
 
     # Get the connected user.
-    user = _get_user(message)
+    user = get_user_from_message(message)
     if user:
         # ---------------------------------------------------------------------
         # TODO: URL for groups looks like: /chat/group/1-test-group/
         # we need to construct a room name for that? And alter the below.
+        #
+        # TODO: OR EVEN BETTER... specify a room in the header or as part
+        # of a message payload, rather than in a url. This allows the mobile
+        # client to use a single websocket connection, rather than a different
+        # connection for each room.
+        #
+        # 1. specify room when connectiong.
+        # 2. specify room as part of message payload?
+        # 3. ... ?
         # ---------------------------------------------------------------------
 
         # 1-1 chat rooms between a logged-in user and a path-defined user.
@@ -292,9 +189,9 @@ def ws_connect(message):
 def ws_disconnect(message):
     """Handles when clients disconnect from a websocket.
     Connected to the `websocket.disconnect` channel."""
-    _log_messages(message.content)
+    log_messages_to_redis(message.content)
 
-    user = _get_user(message)
+    user = get_user_from_message(message)
 
     # Pull the room from the channel's session.
     room = message.channel_session.get('room')
--- tndata_backend/chat/utils.py
+++ tndata_backend/chat/utils.py
@@ -1,9 +1,136 @@
+import json
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.utils.text import slugify
+
+from pprint import pformat
+from django_rq import get_connection
+
+
+def log_messages_to_redis(payload, key='websocket-debug'):
+    """Log all websocket traffic for debugging purposes."""
+    if settings.DEBUG or settings.STAGING:
+        conn = get_connection('default')
+        info = "{}|{}".format(timezone.now().strftime("%c"), pformat(payload))
+        conn.lpush(key, info)
+
+
+def decode_message_text(message):
+    """Given a message, attempt to decode the JSON string and return the TEXT
+    of the message (what the user should see).
+
+    Returns a tuple of the type:
+
+        (message_content, message_type)
+
+    where message type is one of the following:
+
+        - "message": This is a ChatMessage the user should see.
+        - "receipt": This is a read-receipt message (not visible to the user)
+
+    """
+    try:
+        received = json.loads(message.content['text'])
+        if 'received' in received:
+            return (received['received'], 'receipt')
+        return (received['text'], 'message')
+    except (IndexError, ValueError):  # wasnt' JSON-encoded?
+        return message.content.get('text', '')
 
 
 def generate_room_name(values):
     """Given an iterable of values that can either be strings or User objects,
-    generate a room name."""
+    generate a chat room name.
+
+    Usually, this will me a person-to-person chat room, so the format will
+    be something like:  `chat-USERID-USERID`.
+
+    """
     values = [str(v.id) if hasattr(v, 'id') else str(v) for v in values]
     values = sorted(values)
     return slugify("chat-{}".format('-'.join(values)))
+
+
+def get_user_from_message(message):
+    """This function inspects the content of a websocket message and attempts
+    to return the User object who authored the message. There are serveral
+    strategies for doing this:
+
+    1. If `message.user` is an autenticated users, return it.
+    2. See if the User's ID is stored in a channel session and look them up.
+    3. Attempt to lookup an auth token stored in the message's query string
+       or in the headers (available on websocket.connect)
+    4. See if the message's content contains a JSON-encoded string with a
+       token value.
+
+    If any of the above succeed, this function returns a User object. Otherwise,
+    it returns `message.user` which is probably an AnonymousUser object.
+
+    """
+    # 1. Return the authenticated user.
+    if message.user and message.user.is_authenticated():
+        return message.user
+
+    User = get_user_model()
+
+    # 2. Retrieve from channel session.
+    if 'user_id' in message.channel_session:
+        try:
+            return User.objects.get(pk=message.channel_session['user_id'])
+        except User.DoesNotExist:
+            pass
+
+    # 3a. Look for a token in a header
+    token = None
+    try:
+        headers = [
+            (k.decode('utf-8'), v.decode('utf-8'))
+            for k, v in message.content['headers']
+        ]
+        headers = [v for k, v in headers if k.lower() == 'authorization']
+        token = headers[0].split()[1]  # 'Token ACTUAL_TOKEN_STRING'
+    except (KeyError, IndexError, AttributeError):
+        pass
+
+    # 3b. Look for a token in a query string
+    try:
+        # NOTE: this is the only query string object supported.
+        token = message.content.get('query_string').split("token=")[1]
+    except (KeyError, IndexError, AttributeError):
+        pass
+
+    try:
+        if token:
+            return User.objects.get(auth_token__key=token)
+    except User.DoesNotExist:
+        pass
+
+    # 4. See if there's a token or ID in the message content.
+    try:
+        payload = json.loads(message.content.get('text', None))  # Poss. TypeError
+        if 'token' in payload:
+            return User.objects.get(auth_token__key=payload['token'])
+        elif 'from' in payload and payload['from'].isnumeric():
+            return User.objects.get(pk=payload['from'])
+    except (TypeError, IndexError, ValueError, User.DoesNotExist):
+        pass
+
+    # Return None if we didn't find a user. Don't allow Anonymous Users.
+    return None
+
+
+def get_user_details(user):
+    """Given a User, return a tuple of (name, avatar) for inclusion in
+    a Chat Message payload."""
+    try:
+        name = user.get_full_name()
+        avatar = (
+            user.userprofile.google_image.replace('https:', '').replace('http:', '')
+            or '//www.gravatar.com/avatar/0?d=mm&s=30'
+        )
+    except AttributeError:
+        name = "Anonymous"
+        avatar = '//www.gravatar.com/avatar/0?d=mm&s=30'
+    return (name, avatar)
