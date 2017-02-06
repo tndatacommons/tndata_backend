f47c3d4e41ac7e3069ecf1a1a28024d6c5c45903
Brad Montgomery <brad@bradmontgomery.net>
2017-02-06 11:09:07 -0600

    better ui for the chat admin

--- tndata_backend/chat/admin.py
+++ tndata_backend/chat/admin.py
@@ -12,7 +12,13 @@ class ChatGroupAdmin(admin.ModelAdmin):
 
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'room', 'text', 'read', 'created_on')
-    search_fields = ('room', 'text', 'digest')
+    list_display = ('full_name', 'room', 'text', 'read', 'created_on')
+    search_fields = (
+        'room', 'text', 'digest',
+        'user__first_name', 'user__last_name', 'user__email',
+    )
     list_filter = ('read', )
     raw_id_fields = ('user', )
+
+    def full_name(self, obj):
+        return obj.user.get_full_name() or obj.email
